e381792122deea81b28bc00cd0ae40435383789c
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-17 09:13:32 +0000

    Feb 17, 2026, 2:43 PM

--- .vscode/Function_basics.java
+++ .vscode/Function_basics.java
@@ -1,9 +1,9 @@
 public class Function_basics{
     public static void printhelloworld(){
-        System.out.println("Hello world");
+        System.out.println("hi");
     }
     public static void main(String args[]){
         printhelloworld();
-
     }
 }
+
