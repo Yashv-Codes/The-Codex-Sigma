1be9f19da8954609488e16dc8eb11b084b79b219
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-17 09:39:02 +0000

    Feb 17, 2026, 3:09 PM

--- .vscode/Function_basics.java
+++ .vscode/Function_basics.java
@@ -4,17 +4,17 @@ public class Function_basics{
         System.out.println("hi");
         return;
     }
-    public static int calculatesum(){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("Sum = "+ (a+b));
-        sc.close();
-        return 5;
+    public static void calculatesum(int a, int b){
+        int sum = a+b;
+        System.out.println("Sum = "+ sum);
     }
     public static void main(String args[]){
         printhi(); // function call
-        calculatesum();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        calculatesum(a,b);
     }
     
 }
