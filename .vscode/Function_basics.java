1cde76611913ba9baaa445fa4a3ac4b1305aa074
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-17 10:11:06 +0000

    Feb 17, 2026, 3:41 PM

--- .vscode/Function_basics.java
+++ .vscode/Function_basics.java
@@ -8,16 +8,26 @@ public class Function_basics{
         int sum = a+b;
         System.out.println("Sum = "+ sum);
     }
+    public static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
     public static void main(String args[]){
         printhi(); // function call
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        System.out.print("Enter no: ");
+        int x = sc.nextInt();
+        System.out.print("Enter no: ");
+        int y = sc.nextInt();
+        swap(x,y); // arguments or actual parameters
         sc.close();
-        calculatesum(a,b); // arguments or actual parameters
     }
-    
 }
+
+
 
 
 
