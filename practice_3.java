6be40b52dbceabd5f4b8fccd9721143f1e6ccc32
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 15:17:02 +0000

    Feb 19, 2026, 8:47 PM

--- practice_3.java
+++ practice_3.java
@@ -1,10 +1,13 @@
 import java.util.*;
 public class practice_3 {
+    public static double Avg_of_num(double a, double b, double c){
+        return (a+b+c)/3;
+    }
     public static void main(String args[]){
         
-      
+    }
 }
-}
+
 
 
 
