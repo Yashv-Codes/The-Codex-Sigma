4d90d0ea75c4192297e798479f308991ecf6c924
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 15:25:30 +0000

    Feb 19, 2026, 8:55 PM

--- practice_3.java
+++ practice_3.java
@@ -3,6 +3,13 @@ public class practice_3 {
     public static double Avg_of_num(double a, double b, double c){
         return (a+b+c)/3;
     }
+    public static boolean isEven(int n){
+        if(n%2!=0)
+            return false;
+        else
+            return true;
+
+    }
     public static void main(String args[]){
         
     }
