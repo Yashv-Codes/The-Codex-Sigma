50ca14aebd047bd32f67a253e3e185fd4f5530f9
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 15:38:07 +0000

    Feb 19, 2026, 9:08 PM

--- practice_3.java
+++ practice_3.java
@@ -8,14 +8,25 @@ public class practice_3 {
             return false;
         return true;
     }
+    public static boolean isPalindrome(int n){
+        int reverse = 0;
+        while(n>0){
+        reverse = n%10;
+        n /= 10;
+    }
+    if (reverse!=n)
+        return false;
+    return true;
+}
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
-        System.out.println(isEven(n));
+        System.out.println(isPalindrome(n));
         sc.close();
     }
 }
+
 
 
 
