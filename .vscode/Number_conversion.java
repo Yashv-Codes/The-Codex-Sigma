688aaf8224db542c4d9cf32450d256f90a3dcf7c
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 09:57:17 +0000

    Feb 19, 2026, 3:27 PM

--- .vscode/Number_conversion.java
+++ .vscode/Number_conversion.java
@@ -1,4 +1,9 @@
 public class Number_conversion {
-    
+    public static void Binary_to_decimal(int n){
+        int new_num = 0;
+        int pow = 0;
+        int last_digit = n%10;
+        new_num += (last_digit*Math.pow(1,2)); 
+    }
     
 }
