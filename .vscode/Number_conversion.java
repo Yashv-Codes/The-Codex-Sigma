4ea6f532cb6202b67f0c5dd76d27365cd46bebba
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 10:04:43 +0000

    Feb 19, 2026, 3:34 PM

--- .vscode/Number_conversion.java
+++ .vscode/Number_conversion.java
@@ -1,9 +1,24 @@
+import java.util.*;
 public class Number_conversion {
     public static void Binary_to_decimal(int n){
         int new_num = 0;
         int pow = 0;
+        while(n>0){
         int last_digit = n%10;
-        new_num += (last_digit*Math.pow(1,2)); 
+        new_num += (last_digit*Math.pow(2,pow)); 
+        pow++;
+        n /= 10;
     }
-    
+    System.out.println("Decimal = "+new_num);
 }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a no: ");
+        int a = sc.nextInt();
+        sc.close();
+        Binary_to_decimal(a);
+    }
+}
+
+
+
