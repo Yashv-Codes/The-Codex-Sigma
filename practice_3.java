05b01cb0448e6bfad9e9d5c837d52d15d198b98f
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-19 15:12:19 +0000

    Feb 19, 2026, 8:42 PM

--- practice_3.java
+++ practice_3.java
@@ -1,18 +1,9 @@
 import java.util.*;
 public class practice_3 {
     public static void main(String args[]){
-        // Code to calculate sum
-        Scanner ob = new Scanner(System.in);
-        Float radius = ob.nextFloat();
-        // int b = ob.nextInt();
-        // int sum = a+b;
-        // int product = a*b;
-        Float areaofcircle = 3.14f*radius*radius; /* always use f for float, beacuse java always 
-        automatically takes decimal values to double */ 
-        System.out.println(areaofcircle);
-        ob.close();
-    }
-    
+        
+      
+}
 }
 
 
