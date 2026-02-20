11158148a8924bbd76869d869fe9e9bfaa56035c
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-20 09:38:51 +0000

    Feb 20, 2026, 3:08 PM

--- Halfpyramid.java
+++ Halfpyramid.java
@@ -9,9 +9,11 @@ public class Halfpyramid {
         // }
         
         // INVERTED AND ROTATED.
+        int M = 3;
         for(int line=1; line<=n; line++){
-            for(int space=1; space<=n-line; space++){
+            for(int space=1; space<=M; space++){
                 System.out.print(" "+" ");
+                M--;
             }
             for(int stars=1; stars<=line; stars++){
                 System.out.print("*"+ " ");
@@ -20,6 +22,7 @@ public class Halfpyramid {
         }
     }
 }
+
 
  
 
