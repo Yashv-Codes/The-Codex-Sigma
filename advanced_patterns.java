c515fdc874f50b9e51b72da0626fb389f78f0819
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-21 10:31:30 +0000

    Feb 21, 2026, 4:01 PM

--- advanced_patterns.java
+++ advanced_patterns.java
@@ -11,16 +11,52 @@ public class advanced_patterns {
                 System.out.println();
             }
         }
+    public static void Butterfly_pattern(int n){
+        // Upper half
+        int M = 6;
+        for(int line=1; line<=n; line++){
+            for(int star=1; star<=line; star++){
+                System.out.print("*"+" ");
+            }
+            // Spaces
+            for(int space=1; space<=M; space++){
+                System.out.print(" "+ " ");
+            }
+            M -= 2;
+            // Again stars
+            for(int star=1; star<=line; star++){
+                System.out.print("*"+" ");
+            }
+            System.out.println(); // next line
+        }
+        // Second Half
+        int K = 0;
+        for (int line=n; line>=1; line--){
+            for(int star=1; star<=line; star++){
+                System.out.print("*"+ " ");
+            }
+            // Spaces
+            for(int space=1; space<=K; space++){
+                System.out.print(" "+" ");
+            }
+            K += 2;
+            // Again Stars
+            for(int star=1; star<=line; star++){
+                System.out.print("*"+" ");
+            }
+            System.out.println(); // for next line
+        }
+    }
     
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
-        zero_one_pattern(n);
+        Butterfly_pattern(n);
         sc.close();
-
     }
 }
+
 
 
 
