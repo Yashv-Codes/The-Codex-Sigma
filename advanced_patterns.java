b53b3de096745dccc8d23c432c0a14489a2a896c
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-21 12:08:26 +0000

    Feb 21, 2026, 5:38 PM

--- advanced_patterns.java
+++ advanced_patterns.java
@@ -77,7 +77,6 @@ public class Advanced_patterns {
     }
     public static void Diamond_pattern(int n){
         // Upper half
-        
         for(int line=1; line<=n; line++){
             // Spaces
             for(int space=1; space<=n-line; space++){
@@ -110,6 +109,7 @@ public class Advanced_patterns {
         sc.close();
     }
 }
+
 
 
 
