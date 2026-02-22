20070a008ba3319bdc17791d600a9821ea928e1e
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-22 14:22:06 +0000

    Feb 22, 2026, 7:52 PM

--- Array_basics.java
+++ Array_basics.java
@@ -1,23 +1,21 @@
 import java.util.*;
 public class Array_basics {
+    public static void update(int num[]){
+        for(int i=0; i<num.length; i++){
+            num[i]+= 1;
+        }
+    }
     public static void main(String args[]){
-        int marks[]= new int[50];
-        Scanner sc = new Scanner(System.in);
-        marks[0]= sc.nextInt();
-        marks[1]= sc.nextInt();
-        marks[2]= sc.nextInt();
-
-        System.out.println("Physics: "+ marks[0]);
-        System.out.println("Chemistry: "+ marks[1]);
-        System.out.println("Maths: "+marks[2]);
-        marks[2] = marks[2] - 1; // can update array.
-        System.out.println("Maths: "+marks[2]);
-        int percentage = (marks[0]+ marks[1]+ marks[2])/3;
-        System.out.println("Percentage = "+percentage);
-        System.out.println("Length of array = "+marks.length); // LENGTH OF ARRAY.
-        sc.close();
+        int num[]= {24, 45, 89};
+        update(num);
+        for(int i=0; i<num.length; i++){
+            System.out.println(num[i]);
+        }
     }
 }
+
+
+
 
 
 
