07e7564ebd767fd8282c4bb3029f8347645a5b25
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 15:18:06 +0000

    Feb 23, 2026, 8:48 PM

--- prac.java
+++ prac.java
@@ -1,6 +1,15 @@
 public class prac {
     public static void subarrays(int arr[]){
-        
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<=i; j++){
+                System.out.print("(" + arr[i] + "," + arr[j] + ") ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {2,4,6,8,10};
+        subarrays(arr);
     }
     
 }
