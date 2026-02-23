af207fe6afffaf8a6f214b950fe01d63d1a8aab9
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 15:53:00 +0000

    Feb 23, 2026, 9:23 PM

--- arrays_practice.java
+++ arrays_practice.java
@@ -1,6 +1,6 @@
 public class arrays_practice {
     public static void subarray(int arr[]){
-        int end = arr.length-1;
+        int end = arr.length-1; // index starts from 0 that's why len-1.
         for(int start=end; start>=0; start--){
             for(int k=start; k<=end; k++){
                 System.out.print(arr[k]+ " ");
