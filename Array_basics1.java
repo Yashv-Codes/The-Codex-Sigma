239a965429fb826c48a78292e53639fae736e408
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 13:04:25 +0000

    Feb 23, 2026, 6:34 PM

--- Array_basics1.java
+++ Array_basics1.java
@@ -11,12 +11,25 @@ public class Array_basics1 {
             last--;
         }
     }
+    public static void pairs(int arr[]){
+        int count = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                System.out.print((+ i + "," + j ));
+
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String args[]){
         int arr[] = {1,2,3,4,5};
-        reverse_array(arr);
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+ " ");
-        }
+        // reverse_array(arr);
+        // for(int i=0; i<arr.length; i++){
+        //     System.out.print(arr[i]+ " ");
+        // }
+        pairs(arr);
     }
 }
 
