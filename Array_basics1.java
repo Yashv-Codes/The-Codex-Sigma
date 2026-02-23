e1d50ae25c32d56559054b93a20cf2c3dedbab5f
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 12:41:34 +0000

    Feb 23, 2026, 6:11 PM

--- Array_basics1.java
+++ Array_basics1.java
@@ -1,4 +1,26 @@
 public class Array_basics1 {
-    
+    public static void reverse_array(){
+        int arr [] = {1,2,3,4,5};
+        int first = 0;
+        int last = arr.length-1;
+        while(first<last){
+            int temp = arr[first];
+            arr[first] = arr[last];
+            arr[last] = temp;
+
+            first++;
+            last--;
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {1,2,3,4,5};
+        reverse_array();
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        
+    }
     
 }
+
+
