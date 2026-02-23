462068de1a6b997b8dfa3ee38e53af27f6ed56d7
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 13:05:30 +0000

    Feb 23, 2026, 6:35 PM

--- Array_basics1.java
+++ Array_basics1.java
@@ -16,9 +16,11 @@ public class Array_basics1 {
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 System.out.print((+ i + "," + j ));
+                count++;
 
             }
             System.out.println();
+            System.out.println("Total pairs = "+count);
         }
 
     }
