8a1c44f2b52c46b5f5ca10b1c8b10afa6df829b5
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-23 15:05:37 +0000

    Feb 23, 2026, 8:35 PM

--- Arrays.java
+++ Arrays.java
@@ -14,10 +14,8 @@ public class Arrays {
     public static void Subarrays(int arr[]){
         int count = 0;
         for(int i=0; i<arr.length; i++){
-            int start = i;
-            for(int j=i; j<arr.length; j++){
-                int end = j;
-                for(int k=start; k<=end; k++){
+             for(int j=i; j<arr.length; j++){
+                for(int k=i; k<=j; k++){
                     System.out.print( + arr[k] + " ");
                 }
                 count++;
