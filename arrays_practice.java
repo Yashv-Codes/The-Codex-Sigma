1e9f9a8fe0cd8a35ea8566eb25abd461de2760ae
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-24 12:10:15 +0000

    Feb 24, 2026, 5:40 PM

--- arrays_practice.java
+++ arrays_practice.java
@@ -1,12 +1,13 @@
 public class arrays_practice {
     public static void subarray(int arr[]){
-        for(int start=0; start<arr.length-1; start++){
-            for(int end=start; end<arr.length-1; end++){
+        for(int start=0; start<1; start++){
+            for(int end=start; end<arr.length; end++){
                 for(int k=start; k<=end; k++){
                     System.out.print(arr[k]+ " ");
                 }
                 System.out.println();
             }
+            System.out.println();
         }
     }
     public static void main(String[] args){
