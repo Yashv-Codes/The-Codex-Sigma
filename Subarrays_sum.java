09e0c47a39605278165227d15c889579b3bd90a9
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-24 14:42:11 +0000

    Feb 24, 2026, 8:12 PM

--- Subarrays_sum.java
+++ Subarrays_sum.java
@@ -1,20 +1,32 @@
-public class Subarrays_sum {
+public class Subarrays_sum{
     public static void prefix_subarray_sum(int arr[]){
         int prefix[] = new int[arr.length];
+        prefix[0] = arr[0];
         // calculate prefix array
-        for(int start=1; start<prefix.length; start++){
-            prefix[start] = prefix[start-1] + arr[start];
+        for(int i=1; i<prefix.length; i++){
+            prefix[i] = prefix[i-1] + arr[i];
         }
-        // calculate from a specific index you want to
+        // calculate max sum subarray
         int maxsum = Integer.MIN_VALUE;
-        for(int start=0; start<arr.length; start++){
-            for(int end=start; end<arr.length; end++){
-                int currsum = 0;  
-                      
-                    
+        for(int start=3; start<arr.length; start++){
+            for(int end=start; end<6; end++){
+                int currsum = start==0? prefix[end] : prefix[end] - prefix[start-1]; 
+                if(maxsum<currsum) 
+                    maxsum = currsum;
+                }
             }
-
-        }
+        System.out.println("Maxsum = "+maxsum);
     }
-    
+
+    public static void main(String[] args){
+    int arr[] = {2,4,6,8,10,15,67,13};
+    prefix_subarray_sum(arr);
+    }
 }
+
+
+    
+    
+
+
+
