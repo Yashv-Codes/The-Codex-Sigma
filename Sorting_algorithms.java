c77aea19908b4479ce4729c6cb889bec16c02f61
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-02-27 07:08:48 +0000

    Feb 27, 2026, 12:38 PM

--- Sorting_algorithms.java
+++ Sorting_algorithms.java
@@ -19,9 +19,30 @@ public class Sorting_algorithms {
         System.out.println();
     }
 }
+    public static void Selection_sort(int arr[]){
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int min = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[min]>arr[j])
+                    min = j;
+            }
+            // Swap
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         int arr[] = {5, 4, 1, 3, 2};
-        Bubble_sort(arr);
+        Selection_sort(arr);
+        printarr(arr);
     }
 }
 
