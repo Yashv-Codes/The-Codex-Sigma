ef01c64d1f99f75087373800d55096adecb3ddad
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-02 16:44:29 +0000

    Mar 2, 2026, 10:14 PM

--- Sorting_practice.java
+++ Sorting_practice.java
@@ -1,5 +1,17 @@
 public class Sorting_practice {
-    
+    public static void Count_sort(int arr[]){
+        int n = arr.length;
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<n; i++){
+            if(largest<arr[i]){
+                largest = arr[i];
+            }
+            int count[] = new int[largest+1];
+            
+            
+
+        }
+    }
     public static void print_array(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+ " ");
