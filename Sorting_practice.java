756727b484c00f665a529128402869433c566eec
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-02 13:12:21 +0000

    Mar 2, 2026, 6:42 PM

--- Sorting_practice.java
+++ Sorting_practice.java
@@ -1,9 +1,17 @@
 public class Sorting_practice {
     public static void Bubble_sort(int arr[]){
         int n = arr.length;
-        for(int turn=0; turn<n; turn++){
-            
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-2-i; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
         }
     }
-    
 }
+    
+
