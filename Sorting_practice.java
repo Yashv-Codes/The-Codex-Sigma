4c9c626a43fb9a35cdb6579f8af27f705dc90a1f
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-02 16:11:24 +0000

    Mar 2, 2026, 9:41 PM

--- Sorting_practice.java
+++ Sorting_practice.java
@@ -32,9 +32,10 @@ public class Sorting_practice {
             int curr = arr[i];
             int prev = i-1;
             while(prev>=0 && arr[prev]>curr){
-                
+                arr[prev+1] = arr[prev];
+                prev--;
             }
-
+            arr[prev+1] = curr;
         }
     }
     public static void print_array(int arr[]){
@@ -45,10 +46,11 @@ public class Sorting_practice {
     }
     public static void main(String[] args){
         int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
-        Selection_sort(arr);
+        Insertion_sort(arr);
         print_array(arr);
     }
 }
+
 
 
 
