d43002dbb4af4d7924cf5c7202bee8fdf97427cf
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-02 16:15:34 +0000

    Mar 2, 2026, 9:45 PM

--- Sorting_practice.java
+++ Sorting_practice.java
@@ -1,43 +1,5 @@
 public class Sorting_practice {
-    public static void Bubble_sort(int arr[]){
-        int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-1-i; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-    }
-    public static void Selection_sort(int arr[]){
-        int n = arr.length;
-        for(int i=0; i<n-1; i++){
-            int min = i;
-            for(int j=i+1; j<n; j++){
-                if(arr[min]>arr[j]){
-                    min = j;
-                }
-            }
-            int temp = arr[min];
-            arr[min] = arr[i];
-            arr[i] = temp;
-            
-        }
-    }
-    public static void Insertion_sort(int arr[]){
-        int n = arr.length;
-        for(int i=0; i<n; i++){
-            int curr = arr[i];
-            int prev = i-1;
-            while(prev>=0 && arr[prev]>curr){
-                arr[prev+1] = arr[prev];
-                prev--;
-            }
-            arr[prev+1] = curr;
-        }
-    }
+    
     public static void print_array(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+ " ");
@@ -46,7 +8,6 @@ public class Sorting_practice {
     }
     public static void main(String[] args){
         int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
-        Insertion_sort(arr);
         print_array(arr);
     }
 }
