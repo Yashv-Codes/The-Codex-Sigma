6a6b04d375b6a9633c499095582ab38dd4f7f244
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-04 10:55:34 +0000

    Mar 4, 2026, 4:25 PM

--- Spiral_Matrix.java
+++ Spiral_Matrix.java
@@ -1,3 +1,24 @@
 public class Spiral_Matrix {
+    public static void printspiral(int matrix[][]){
+        int srow = 0;
+        int erow = matrix.length-1;
+        int scol = 0;
+        int ecol = matrix[0].length-1;
+
+        while(srow<=erow && scol<=ecol){
+            // Top part
+            for(int j=scol; j<=ecol; j++){
+                System.out.print(matrix[srow][j]);
+            }
+            // Right part
+            
+        }
+
+
+    }
+    public static void main(String[] args){
+        int matrix[][] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+
+    } 
     
 }
