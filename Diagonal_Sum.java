2c2f3332fc123e61812844d111d8899b4e40a111
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-04 13:04:19 +0000

    Mar 4, 2026, 6:34 PM

---
+++ Diagonal_Sum.java
@@ -0,0 +1,23 @@
+public class Diagonal_Sum {
+    public static int printsum(int matrix[][]){
+        int sum = 0;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                // Primary Diagonal
+                if(i == j)
+                    sum += matrix[i][j];
+                // Secondary Diagonal
+                else if(i+j == matrix.length-1)
+                    sum += matrix[i][j];
+                 }
+            }
+            return sum;
+        }
+        public static void main(String[] args){
+            int matrix[][] = {{1,2,3,4},
+                          {5,6,7,8}, 
+                          {9,10,11,12}, 
+                          {13,14,15,16}};
+            printsum(matrix);
+        }
+    }
