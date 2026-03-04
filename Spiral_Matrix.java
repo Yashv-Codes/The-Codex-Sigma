7caefe3ca3132286073aff8a13f4af172024c2a6
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-04 11:28:25 +0000

    Mar 4, 2026, 4:58 PM

--- Spiral_Matrix.java
+++ Spiral_Matrix.java
@@ -8,17 +8,33 @@ public class Spiral_Matrix {
         while(srow<=erow && scol<=ecol){
             // Top part
             for(int j=scol; j<=ecol; j++){
-                System.out.print(matrix[srow][j]);
+                System.out.print(matrix[srow][j]+ " "); // j->column because going from scol->ecol.
             }
             // Right part
-            
+            for(int i=srow+1; i<=erow; i++){ 
+                System.out.print(matrix[ecol][i]+" "); // i->row because going from srow->erow.
+            }
+            // Bottom part
+            for(int j=ecol-1; j<=scol; j++){
+                System.out.print(matrix[erow][j]+" ");
+            }
+            // Left part
+            for(int i=erow-1; i<=srow+1; i++){
+                System.out.print(matrix[scol][i]+" ");
+            }
+            srow++;
+            erow--;
+            scol++;
+            ecol--;
+            System.out.println();
+
         }
-
-
     }
     public static void main(String[] args){
         int matrix[][] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}};
+        printspiral(matrix);
 
     } 
-    
 }
+
+
