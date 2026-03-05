bc7d23dde4a62540c75e74a8c2a76ce80e1a9fe7
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-05 16:37:23 +0000

    Mar 5, 2026, 10:07 PM

--- Assignment4.java
+++ Assignment4.java
@@ -6,17 +6,30 @@ public class Assignment4 {
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == key)
                     count++;
-                
                 }
             }
             return count;
         }
+        // To print sum of second row of matrix.
+        public static int sumofrow(int matrix[][]){
+            int sum = 0;
+            for(int j=0; j<matrix[0].length; j++){
+                sum += matrix[1][j];
+            }
+            return sum;
+        }
         public static void main(String[] args){
             int matrix[][] = {{1,4,9}, {11,4,3}, {2,2,3}};
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter key = ");
-            int key = sc.nextInt();
-            System.out.println("Key appeared: "+printcount(matrix,key));
+            // Scanner sc = new Scanner(System.in);
+            // System.out.print("Enter key = ");
+            // int key = sc.nextInt();
+            // System.out.println("Key appeared: "+printcount(matrix,key));
+            // sc.close();
+            System.out.println("Sum of row = "+sumofrow(matrix));
+
         }
     }
+    
+
+
 
