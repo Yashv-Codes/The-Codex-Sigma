d19dabb4b571776c8c884acee735c704e96d585e
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-05 16:30:01 +0000

    Mar 5, 2026, 10:00 PM

--- Assignment4.java
+++ Assignment4.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Assignment4 {
     public static int printcount(int matrix[][], int key){
         int count = 0;
@@ -5,9 +6,17 @@ public class Assignment4 {
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == key)
                     count++;
+                
                 }
             }
             return count;
         }
-        public static void main(String[] args)
+        public static void main(String[] args){
+            int matrix[][] = {{1,4,9}, {11,4,3}, {2,2,3}};
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter key = ");
+            int key = sc.nextInt();
+            System.out.println("Key appeared: "+printcount(matrix,key));
+        }
     }
+
