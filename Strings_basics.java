f546c9034f7a3810a56413c44d9eda0913c5a6ae
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-06 14:17:04 +0000

    Mar 6, 2026, 7:47 PM

--- Strings_basics.java
+++ Strings_basics.java
@@ -1,8 +1,14 @@
 import java.util.*;
 public class Strings_basics {
+    public static void printletters(String name){
+        for(int i=0; i<name.length(); i++){
+            System.out.print(name.charAt(i));
+        }
+        System.out.println();
+    }
     public static void main(String[] args){
         char arr[] = {'a', 'b', 'c', 'd'};
-        String str = "yash verma";
+        String str = "Yash Verma";
         String str1 = new String("xyz");
         // Scanner sc = new Scanner(System.in);
         // System.out.print("Enter a string: ");
@@ -13,11 +19,15 @@ public class Strings_basics {
         System.out.println(str.length());
 
         // Concatenation
-        String str2 = str+str1;
-        System.out.println(str2); 
-        System.out.println(str2.charAt(0));
+        // String str2 = str+str1;
+        // System.out.println(str2); 
+        // System.out.println(str2.charAt(0));
+
+        String name = "Kisna Tailor";
+        printletters(name);
     }
 }
+
 
 
 
