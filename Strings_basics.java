1e7b638b225f3e06b4983923c8b6b84cb74f32c2
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-06 13:50:38 +0000

    Mar 6, 2026, 7:20 PM

---
+++ Strings_basics.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Strings_basics {
+    public static void main(String[] args){
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "yash";
+        String str1 = new String("xyz");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String name = sc.next();
+        System.out.println("You entered: "+name);
+        sc.close();
+
+    }
+}
+
+
