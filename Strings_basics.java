3fbf43e3f00716f7e4aa84d9468d148d702ec88f
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-06 13:59:18 +0000

    Mar 6, 2026, 7:29 PM

--- Strings_basics.java
+++ Strings_basics.java
@@ -2,16 +2,18 @@ import java.util.*;
 public class Strings_basics {
     public static void main(String[] args){
         char arr[] = {'a', 'b', 'c', 'd'};
-        String str = "yash";
+        String str = "yash verma";
         String str1 = new String("xyz");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String name = sc.nextLine();
-        System.out.println("You entered: "+name);
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a string: ");
+        // String name = sc.nextLine();
+        // System.out.println("You entered: "+name);
+        // sc.close();
 
+        System.out.println(str.length());
     }
 }
+
 
 
 
