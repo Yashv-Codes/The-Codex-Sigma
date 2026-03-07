c5ae62d756d6cf4e8fb7dc5eb9e5dd057dfa4aa6
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-07 17:18:17 +0000

    Mar 7, 2026, 10:48 PM

--- Substring.java
+++ Substring.java
@@ -1,6 +1,14 @@
 public class Substring {
+    public static String printsubstr(String str, int si, int ei){
+        String substr = "";
+        for(int i=si; i<ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
     public static void main(String[] args){
         String str = "Hello World";
+        System.out.println(printsubstr(str,0,5));
         
     }
     
