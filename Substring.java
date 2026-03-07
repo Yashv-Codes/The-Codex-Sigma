ead8e52a68030ca5d5414e70a514244ca4a51ee4
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-07 17:22:20 +0000

    Mar 7, 2026, 10:52 PM

--- Substring.java
+++ Substring.java
@@ -8,7 +8,8 @@ public class Substring {
     }
     public static void main(String[] args){
         String str = "Hello World";
-        System.out.println(printsubstr(str,0,5));
+        // System.out.println(printsubstr(str,0,5));
+        System.out.println(str.substring(0,5));
         
     }
     
