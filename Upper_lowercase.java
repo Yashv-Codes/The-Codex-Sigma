609635abbe41ec6793a56e9cdfb456072e462ec5
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-08 15:22:47 +0000

    Mar 8, 2026, 8:52 PM

---
+++ Upper_lowercase.java
@@ -0,0 +1,10 @@
+public class Upper_lowercase {
+    public static void toUpper(String str){
+        
+
+    }
+    public static void main(String[] args){
+        String str = "hi, i am yash";
+    }
+    
+}
