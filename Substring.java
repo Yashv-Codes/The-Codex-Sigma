ca39683349084cc9ef361474d7d4b8cd80dcdfc2
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-08 15:01:32 +0000

    Mar 8, 2026, 8:31 PM

--- Substring.java
+++ Substring.java
@@ -11,18 +11,23 @@ public class Substring {
         // System.out.println(printsubstr(str,0,5));
         System.out.println(str.substring(0,5));
 
-        String fruits[] = {"apple", "mango", "banana"};
-        String largest = fruits[0];
-        for(int i=0; i<fruits.length; i++){
-            if(largest.compareTo(fruits[i])<0){
-                largest = fruits[i];
-            }
-        }
-        System.out.println(largest);
+        // String fruits[] = {"apple", "mango", "banana"};
+        // String largest = fruits[0];
+        // for(int i=0; i<fruits.length; i++){
+        //     if(largest.compareTo(fruits[i])<0){
+        //         largest = fruits[i];
+        //     }
+        // }
+        // System.out.println(largest);
 
         // Stringbuilder
-        
+        StringBuilder sb = new StringBuilder("");
+        for(char ch='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
     }
 }
+
 
 
