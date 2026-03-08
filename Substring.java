9519e474eaf39bcd0db87f725c6ffe68b1f26a95
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-08 13:06:38 +0000

    Mar 8, 2026, 6:36 PM

--- Substring.java
+++ Substring.java
@@ -10,9 +10,16 @@ public class Substring {
         String str = "Hello World";
         // System.out.println(printsubstr(str,0,5));
         System.out.println(str.substring(0,5));
+
         String fruits[] = {"apple", "mango", "banana"};
         String largest = fruits[0];
-        
+        for(int i=0; i<fruits.length; i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
     }
 }
+
 
