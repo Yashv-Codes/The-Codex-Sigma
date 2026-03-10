d6092f2f7c17d624c5a79a0e5e464bc74ccb4a16
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-10 15:37:28 +0000

    Mar 10, 2026, 9:07 PM

--- String_compression.java
+++ String_compression.java
@@ -1,14 +1,14 @@
 public class String_compression {
     public static String tocompress(String str){
         StringBuilder sb = new StringBuilder("");
-        sb.append(str.charAt(0));
+        // sb.append(str.charAt(0));
         for(int i=0; i<str.length(); i++){
             Integer count = 1;
-            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){ // while loop is for count only.
                 count++;
                 i++;
             }
-            sb.append(str.charAt(i));
+            sb.append(str.charAt(i)); // will print current value of i.
             if(count > 1){
                 sb.append(count.toString());
             }
@@ -20,4 +20,6 @@ public class String_compression {
         System.out.println(tocompress(str));
     }
 }
+
+
 
