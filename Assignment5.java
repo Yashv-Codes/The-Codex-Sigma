33f23f233cb8ee078238f7347c9b060be0e88683
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-10 15:57:24 +0000

    Mar 10, 2026, 9:27 PM

--- Assignment5.java
+++ Assignment5.java
@@ -14,7 +14,7 @@ public class Assignment5 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String str = sc.next();
+        String str = sc.nextLine();
         System.out.println("Total vowels occured: "+count_vowels(str));
     }
 }
