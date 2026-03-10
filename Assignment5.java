4e7c95850164f8418002381f0e8f1b4fd3968c83
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-10 16:15:37 +0000

    Mar 10, 2026, 9:45 PM

--- Assignment5.java
+++ Assignment5.java
@@ -12,10 +12,14 @@ public class Assignment5 {
         return sb.toString();
     }
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-        System.out.println("Total vowels occured: "+count_vowels(str));
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a string: ");
+        // String str = sc.nextLine();
+        // System.out.println("Total vowels occured: "+count_vowels(str));
+        String str = "ShradhaDidi";
+        String str1 = "ApnaCollege";
+        String str2 = "ShradhaDidi";
+        System.out.println(str.equals(str1) + " " + str.equals(str2));
     }
 }
 
