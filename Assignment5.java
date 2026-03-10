79e1cc0e208da23df4476157ae76ec02ca8af4ef
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-10 15:56:41 +0000

    Mar 10, 2026, 9:26 PM

--- Assignment5.java
+++ Assignment5.java
@@ -1,4 +1,21 @@
+import java.util.*;
 public class Assignment5 {
-    
-    
+    public static String count_vowels(String str){
+        StringBuilder sb = new StringBuilder("");
+        Integer count = 0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u'){
+                count++;
+            }
+        }
+        sb.append(count.toString());
+        return sb.toString();
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+        System.out.println("Total vowels occured: "+count_vowels(str));
+    }
 }
+
