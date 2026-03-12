5f76a4ad50cc64c14e77a8f529d6b354777af8a6
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-12 13:59:47 +0000

    Mar 12, 2026, 7:29 PM

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -10,23 +10,23 @@ public class Bit_Manipulation{
     }
     public static void main(String[] args){
         // Bit-Wise AND
-        // System.out.println((5 & 6));
+        System.out.println((5 & 6));
 
-        // // Bit-Wise OR
-        // System.out.println((5 | 6));
+        // Bit-Wise OR
+        System.out.println((5 | 6));
 
-        // // Bit-Wise XOR
-        // System.out.println((5 ^ 6));
+        // Bit-Wise XOR
+        System.out.println((5 ^ 6));
 
-        // // Binary one's complement
-        // System.out.println((~0));
-        // System.out.println((~5));
+        // Binary one's complement
+        System.out.println((~0));
+        System.out.println((~5));
 
-        // // Binary Left Shift
-        // System.out.println((5 << 2));
+        // Binary Left Shift
+        System.out.println((5 << 2));
 
-        // // Binary Right Shift
-        // System.out.println((6 >> 1));
+        // Binary Right Shift
+        System.out.println((6 >> 1));
 
         OddorEven(4);
         OddorEven(7);
---
+++ ith_bit.java
@@ -0,0 +1,6 @@
+public class ith_bit {
+    public static void Get_ith(int n){
+        
+    }
+    
+}
