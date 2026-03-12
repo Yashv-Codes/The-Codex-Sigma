b93aa65fcf2c6e8e583e80898ad96ee92a583551
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-12 15:05:48 +0000

    Mar 12, 2026, 8:35 PM

--- ith_bit.java
+++ ith_bit.java
@@ -29,14 +29,22 @@ public class ith_bit {
         int bitmask = newbit<<i;
         return n | bitmask;
     }
+
+    public static int Clear_last_ith(int n, int i){
+        int bitmask = (-1)<<i;
+        return n & bitmask;
+    }
+    
     public static void main(String[] args){
         // System.out.println(Get_ith(10,2));
         // System.out.println(Set_ith(5,2));
         // System.out.println(Set_ith(6,3));
-        System.out.println(Update_ith(10,2,2));
-        
+        // System.out.println(Update_ith(10,2,2));
+        System.out.println(Clear_last_ith(15,2));
     }
 }
+
+
 
 
 
