4b7f499f87226d04e20de1150415a758b9873056
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-12 16:23:10 +0000

    Mar 12, 2026, 9:53 PM

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -8,30 +8,42 @@ public class Bit_Manipulation{
             System.out.println("It's odd");
         }
     }
+    public static boolean isPowerofTwo(int n){
+        if(n==0)
+            return false;
+        else if((n & (n-1)) == 0){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         // Bit-Wise AND
-        System.out.println((5 & 6));
+        // System.out.println((5 & 6));
 
-        // Bit-Wise OR
-        System.out.println((5 | 6));
+        // // Bit-Wise OR
+        // System.out.println((5 | 6));
 
-        // Bit-Wise XOR
-        System.out.println((5 ^ 6));
+        // // Bit-Wise XOR
+        // System.out.println((5 ^ 6));
 
-        // Binary one's complement
-        System.out.println((~0));
-        System.out.println((~5));
+        // // Binary one's complement
+        // System.out.println((~0));
+        // System.out.println((~5));
 
-        // Binary Left Shift
-        System.out.println((5 << 2));
+        // // Binary Left Shift
+        // System.out.println((5 << 2));
 
-        // Binary Right Shift
-        System.out.println((6 >> 1));
+        // // Binary Right Shift
+        // System.out.println((6 >> 1));
 
-        OddorEven(4);
-        OddorEven(7);
+        // OddorEven(4);
+        // OddorEven(7);
+
+        System.out.println(isPowerofTwo(0));
+        System.out.println(isPowerofTwo(16));
     }
 }
+
 
 
 
