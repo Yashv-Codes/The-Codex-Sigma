7016729faabd9fb62cda43e7ff1e0bcbcd5af466
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-12 13:49:39 +0000

    Mar 12, 2026, 7:19 PM

--- Bit_Manipulation.java
+++ Bit_Manipulation.java
@@ -1,23 +1,38 @@
 public class Bit_Manipulation{
+    public static void OddorEven(int n){
+        int bitmask = 1;
+        if((n & bitmask) == 0){
+            System.out.println("It's even");
+        }
+        else{
+            System.out.println("It's odd");
+        }
+    }
     public static void main(String[] args){
         // Bit-Wise AND
-        System.out.println((5 & 6));
+        // System.out.println((5 & 6));
 
-        // Bit-Wise OR
-        System.out.println((5 | 6));
+        // // Bit-Wise OR
+        // System.out.println((5 | 6));
 
-        // Bit-Wise XOR
-        System.out.println((5 ^ 6));
+        // // Bit-Wise XOR
+        // System.out.println((5 ^ 6));
 
-        // Binary one's complement
-        System.out.println((~0));
-        System.out.println((~5));
+        // // Binary one's complement
+        // System.out.println((~0));
+        // System.out.println((~5));
 
-        // Binary Left Shift
-        System.out.println((5 << 2));
+        // // Binary Left Shift
+        // System.out.println((5 << 2));
 
-        // Binary Right Shift
-        System.out.println((6 >> 1));
+        // // Binary Right Shift
+        // System.out.println((6 >> 1));
+
+        OddorEven(4);
+        
     }
 }
+
+
+
 
