e325cdd07e3312422f6cee10e868c766ed10d0a6
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-12 14:20:17 +0000

    Mar 12, 2026, 7:50 PM

--- ith_bit.java
+++ ith_bit.java
@@ -5,14 +5,21 @@ public class ith_bit {
             return 0;
         else
             return 1;
-        }
+    }
+
     public static int Set_ith(int n, int i){
         int bitmask = 1<<i;
         return n | bitmask;
     }
+
+    public static int Clear_ith(int n, int i){
+        int bitmask = ~(1<<i);
+        return n & bitmask;
+    }
     public static void main(String[] args){
         // System.out.println(Get_ith(10,2));
-        System.out.println(Set_ith(5,2));
+        // System.out.println(Set_ith(5,2));
+        System.out.println(Set_ith(6,3));
         
     }
 }
