b835401bb2f5c20892bd3513d3b67ac48e09863e
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-16 14:20:33 +0000

    Mar 16, 2026, 7:50 PM

--- Assignment6.java
+++ Assignment6.java
@@ -1,10 +1,19 @@
 public class Assignment6 {
-    public static void swaptwonum(int a, int b){
-        
+    public static void swaptwonum(){
+        int a = 20, b = 30;
+        a = a^b;
+        b = a^b;
+        a = a^b;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
 
     }
     public static void main(String[] args){
         System.out.println(1^1);
+        swaptwonum();
+        
+        
     }
-    
 }
+
+
