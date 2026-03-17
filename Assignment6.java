ac85e232ac631eebd9c4b83fc83a526920b73618
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-17 13:05:59 +0000

    Mar 17, 2026, 6:35 PM

--- Assignment6.java
+++ Assignment6.java
@@ -8,12 +8,27 @@ public class Assignment6 {
         System.out.println("b = "+b);
 
     }
+    public static void toUppercase(){
+        for(char ch='A'; ch<='Z'; ch++){
+            System.out.print((char)(ch | ' '));
+        }
+        System.out.println();
+    }
+    public static void toLowercase(){
+        for(char ch='a'; ch<='z'; ch++){
+            System.out.print((char)(ch & '_'));
+        }
+    }
     public static void main(String[] args){
         System.out.println(1^1);
-        swaptwonum();
+        // swaptwonum();
+        toUppercase();
+        toLowercase();
         
         
     }
 }
+
+
 
 
