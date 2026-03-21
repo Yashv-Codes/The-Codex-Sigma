00a56b3bb7c4a0280fa6ae2644c9b347e2d3943a
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-21 16:00:36 +0000

    Mar 21, 2026, 9:30 PM

--- Inheritance.java
+++ Inheritance.java
@@ -5,12 +5,12 @@ public class Inheritance {
         // lionfish.breathe();
         // lionfish.swim();
 
-        Dog pitbull = new Dog();
-        pitbull.legs = 4;
-        pitbull.eat();
-        pitbull.breed = "brown";
-        System.out.println(pitbull.legs);
-        System.out.println(pitbull.breed);
+        // Dog pitbull = new Dog();
+        // pitbull.legs = 4;
+        // pitbull.eat();
+        // pitbull.breed = "brown";
+        // System.out.println(pitbull.legs);
+        // System.out.println(pitbull.breed);
     }
 }
 
@@ -26,20 +26,39 @@ class Animal{
 }
 
 // Derived class
-class Fish extends Animal{
-    int fins;
-    void swim(){
-        System.out.println("swims in water");
-    }
-}
+
+// class Fish extends Animal{
+//     int fins;
+//     void swim(){
+//         System.out.println("swims in water");
+//     }
+// }
 
 // Multilvl Inheritance
-class Mammal extends Animal{
-    int legs;
-}
+
+// class Mammal extends Animal{
+//     int legs;
+// }
 
 class Dog extends Mammal{
     String breed;
+}
+
+// Hierarchial Inheritance
+class Mammal extends Animal{
+    void walk(){
+    System.out.println("walks");
+    }
+}
+class Fish extends Animal{
+    void swim(){
+        System.out.println("swims");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("flies");
+    }
 }
 
 
