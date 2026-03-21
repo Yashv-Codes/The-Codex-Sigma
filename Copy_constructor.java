af06475920f92564e808bd9831a4a0fcd39b269d
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-21 05:45:12 +0000

    Mar 21, 2026, 11:15 AM

--- Copy_constructor.java
+++ Copy_constructor.java
@@ -12,13 +12,16 @@ public class Copy_constructor {
 class Student{
     String name;
     int roll;
+    int marks[];
 
     Student(){
+        marks = new int[3];
         System.out.println("constructor is called");
     }
 
     // copy constructor
     Student(Student s1){
+        marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
     }
