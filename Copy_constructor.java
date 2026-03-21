c4a349d8fbd6db39db826d05c6c1df8f85490bb4
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-21 06:14:18 +0000

    Mar 21, 2026, 11:44 AM

--- Copy_constructor.java
+++ Copy_constructor.java
@@ -27,11 +27,22 @@ class Student{
     }
 
     //  Shallow copy constructor
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks = s1.marks;
+    // }
+
+    // Deep copy constructor
     Student(Student s1){
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
-        this.marks = s1.marks;
+        for(int i=0; i<marks.length; i++){
+            this.marks[i] = s1.marks[i];
+        }
+
     }
 }
 
