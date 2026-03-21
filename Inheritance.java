5472560969b4997fd7350ae10482b4b35e4baff8
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-21 06:54:37 +0000

    Mar 21, 2026, 12:24 PM

--- Inheritance.java
+++ Inheritance.java
@@ -2,11 +2,11 @@ public class Inheritance {
     public static void main(String[] args){
         Fish lionfish = new Fish();
         lionfish.eat();
+        lionfish.breathe();
         lionfish.swim();
-
     }
-    
 }
+
 // Base class
 class Animal{
     String skincolor;
@@ -25,5 +25,6 @@ class Fish extends Animal{
         System.out.println("swims in water");
     }
 }
+
 
 
