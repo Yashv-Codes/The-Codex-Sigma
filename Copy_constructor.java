a7c76aa5d19ccf0aceff74a09483a5a733161a9e
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-21 05:38:49 +0000

    Mar 21, 2026, 11:08 AM

--- Copy_constructor.java
+++ Copy_constructor.java
@@ -1,4 +1,13 @@
 public class Copy_constructor {
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.name = "yash";
+        s1.roll = 456;
+    }
     
+}
+class Student{
+    String name;
+    int roll;
     
 }
