06c5ddb74147b1c38034f8ac8d992238eb9445c6
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-24 15:41:34 +0000

    Mar 24, 2026, 9:11 PM

--- Polymorphism.java
+++ Polymorphism.java
@@ -4,7 +4,21 @@ public class Polymorphism {
     }
 
     class Calculator{
-        
+        int sum(int a, int b){
+            return a+b;
+        }
+
+        float sum(float a, float b){
+            return a+b;
+        }
+
+        // double subtract(double a, double b){
+        //     return a - b;
+        // }
+
+        int sum(int a, int b, int c){
+            return a+b+c;
+        }
     }
     
 }
