182170f28387844a050a9fb453d94c3177c7d1a1
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-25 06:27:28 +0000

    Mar 25, 2026, 11:57 AM

--- Abstraction.java
+++ Abstraction.java
@@ -1,4 +1,27 @@
 public class Abstraction {
-    
-    
+    public static void main(String[] args){
+        
+
+    }
+
+    abstract class Animal{
+        void eat(){
+            System.out.println("eats");
+        }
+        abstract void walk();
+    }
+
+    class Horse extends Animal{
+        void walk(){
+            System.out.println("walks with legs");
+        }
+    }
+
+    class Chicken extends Horse{
+        void run(){
+            System.out.println("runs with chicken legs");
+        }
+    }
+
 }
+
