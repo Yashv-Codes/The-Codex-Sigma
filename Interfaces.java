0dfe7737487d90c5fb2ee809a36a4d80abb89370
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-25 11:48:43 +0000

    Mar 25, 2026, 5:18 PM

--- Interfaces.java
+++ Interfaces.java
@@ -1,8 +1,30 @@
 public class Interfaces {
     public static void main(String[] args){
+        Queen q = new Queen();
+        
 
     }
     
 }
 
+interface Chessplayer{
+    void moves();
+}
 
+class Queen implements Chessplayer{
+    public void moves(){
+        System.out.println("moves in all directions and diagnols");
+    }
+}
+
+class King implements Chessplayer{
+    public void moves(){
+        System.out.println("moves in all directions by single step at a time");
+    }
+}
+
+class Rook implements Chessplayer{
+    public void moves(){
+        System.out.println("moves horizontally and vertically");
+    }
+}
