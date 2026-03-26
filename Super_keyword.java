4c13f47da8af4aea2300d52b1979a831dab9b8f4
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-26 12:49:21 +0000

    Mar 26, 2026, 6:19 PM

---
+++ Super_keyword.java
@@ -0,0 +1,22 @@
+public class Super_keyword {
+    public static void main(String args[]){
+        Horse h = new Horse();
+
+    }
+    
+}
+
+class Animal{
+    String name;
+    Animal(){
+        System.out.println("animal constructor is called");
+    }
+}
+
+class Horse extends Animal{
+    Horse(){
+        super();
+        System.out.println("horse constructor is called");
+    }
+
+}
