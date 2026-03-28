74efe7af2d55078a4393792438a55d47c01ded6e
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-03-28 13:41:15 +0000

    Mar 28, 2026, 7:11 PM

---
+++ Recursion.java
@@ -0,0 +1,18 @@
+public class Recursion {
+
+    // Print numbers in decreasing order
+    public static void printdec(int n){
+        if(n==1){ // BASE CASE
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printdec(n-1);
+    }
+    public static void main(String[] args){
+        printdec(10);
+
+    }
+    
+}
+
