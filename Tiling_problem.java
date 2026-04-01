3ab0d94c67239a9aa7af01110fe03d455d37767a
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-01 12:44:38 +0000

    Apr 1, 2026, 6:14 PM

--- Tiling_problem.java
+++ Tiling_problem.java
@@ -1,3 +1,20 @@
 public class Tiling_problem {
-    
+    public static int countways(int n){ // 2 X N Floor
+        if(n==0 || n==1)
+            return 1;
+        
+        // Vertical choice
+        int fm1 = countways(n-1);
+
+        // Horizontal choice
+        int fm2 = countways(n-2);
+
+        return fm1 + fm2;
+    }
+
+
+public static void main(String[] args){
+    System.out.println("Total ways = "+countways(5));
 }
+}
+
