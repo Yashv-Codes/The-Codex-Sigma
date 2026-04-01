eba42d629ad51aaf5d29fb8e2d726494486feb16
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-01 14:10:24 +0000

    Apr 1, 2026, 7:40 PM

---
+++ DuplicatesinString.java
@@ -0,0 +1,24 @@
+public class DuplicatesinString {
+    public static void removeDuplicates(String str, int idx, StringBuilder newstr, boolean map[]){
+        if(idx == str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        char currchar = str.charAt(idx);
+        if(map[currchar-'a']==true){ 
+            // Duplicate
+            removeDuplicates(str,idx+1,newstr,map);
+        }
+        else{
+            map[currchar-'a'] = true;
+            removeDuplicates(str,idx+1,newstr.append(currchar),map);
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "aapnnaccollegge";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+
+    }
+}
+
