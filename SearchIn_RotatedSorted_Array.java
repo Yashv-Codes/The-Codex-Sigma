bbd94c7f767cc30034599d4acbd4c34bbe24cc4d
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-15 14:55:08 +0000

    Apr 15, 2026, 8:25 PM

--- SearchIn_RotatedSorted_Array.java
+++ SearchIn_RotatedSorted_Array.java
@@ -36,9 +36,10 @@ public class SearchIn_RotatedSorted_Array {
 
     public static void main(String[] args){
         int arr[] = {4, 5, 6, 7, 0, 1, 2};
-        int target = 0;
+        int target = 7;
         int taridx = search(arr, target, 0, arr.length-1);
         System.out.println("Target found at index : "+taridx);
     }
 }
+
 
