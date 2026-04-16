debc58a35136dd99f4f26ada34103ba7993640c8
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-16 14:23:02 +0000

    Apr 16, 2026, 7:53 PM

--- Assignment7.java
+++ Assignment7.java
@@ -1,3 +1,19 @@
+public class Assignment7{
+    public static void occurence(int arr[], int key, int i){
+        if(i == arr.length)
+            return;
+
+        if(arr[i] == key)
+            System.out.print(i + " ");
+        
+        occurence(arr, key, i+1);
+    }
+
+    public static void main(String[] args){
+        int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
+        occurence(arr, 2, 0);
+    }
+}
 
     
 
