d5fba8d0045f49414b645d1c9f0d5945e21efd7d
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-16 14:47:17 +0000

    Apr 16, 2026, 8:17 PM

--- Assignment7.java
+++ Assignment7.java
@@ -11,11 +11,19 @@ public class Assignment7{
     }
 
     // Convert digits to String
-    
+    public static void printdigits(int n){
+        if(n == 0)
+            return;
+        String digits[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        int lastdigit = n%10;
+        printdigits(n/10);
+        System.out.print(digits[lastdigit]+" ");
+    }
 
     public static void main(String[] args){
         int arr[] = {3, 2, 4, 5, 6, 2, 7, 2, 2};
-        occurence(arr, 2, 0);
+        // occurence(arr, 2, 0);
+        printdigits(2011);
     }
 }
 
