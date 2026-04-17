18e92c32d19253f952b373275b11bb9c57c2c061
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-04-17 14:39:42 +0000

    Apr 17, 2026, 8:09 PM

--- Assignment7.java
+++ Assignment7.java
@@ -31,7 +31,7 @@ public class Assignment7{
     // Substring starting/ending with same character
     public static void printsubstring(String str){
         for(int i=0; i<str.length(); i++){
-            for(int j=i+1; j<str.length(); j++){
+            for(int j=i+1; j<=str.length(); j++){
                 System.out.println(str.substring(i,j));
             }
         }
@@ -46,6 +46,7 @@ public class Assignment7{
         printsubstring(str);
     }
 }
+
 
 
 
