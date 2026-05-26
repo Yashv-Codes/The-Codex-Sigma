2f63b7b3df77322808b22bac99435750d7ba9b86
Yash Verma <258442749+Yashv-Codes@users.noreply.github.com>
2026-05-26 10:16:53 +0000

    May 26, 2026, 3:46 PM

---
+++ Backtracking_Arrays.java
@@ -0,0 +1,26 @@
+public class Backtracking_Arrays{
+    public static void Changearr(int arr[], int i, int val){
+        // Base case
+        if(i == arr.length){
+            printarr(arr);
+            return;
+        }
+        // Recursion
+        arr[i] = val;
+        Changearr(arr, i+1, val+1); 
+        arr[i] -= 2; // Backtacking step
+    }
+
+    public static void printarr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int arr[] = new int[5];
+        Changearr(arr, 0, 1);
+        printarr(arr);
+    }
+}
