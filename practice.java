831331d2cb68349bff8ca1c6fd1bdf451f213e98
UNDISPUTED <devverma@Devs-MacBook-Air.local>
2026-08-21 21:49:21 +0530

    Auto update

---
+++ practice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class practice{
+    public static void main(String[] args){
+        String type = ((5%2) == 0) ? "even" : "odd";
+        System.out.println(type);
+       
+
+
+        
+         
+        
+    }
+}
