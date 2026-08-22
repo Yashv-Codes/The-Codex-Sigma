bdeb9224f139959836da43569017666168c9dca0
Yashv-Codes <yashv5529@gmail.com>
2026-08-22 14:22:26 +0000

    DSA - 22 Aug 2026, 19:52

--- practice.java
+++ practice.java
@@ -1,8 +1,7 @@
 public class practice{
     public static void main(String[] args){
         System.out.println("Hi");
-        System.out.println();
-        System.out.println();
+        
         
         
         
