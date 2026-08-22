b4f24bbd0ba4a33748097ba641f0de3f21a8ab38
Yashv-Codes <yashv5529@gmail.com>
2026-08-22 15:44:38 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,8 +1,6 @@
 public class practice{
     public static void main(String[] args){
-        System.out.println("Hi");
-        int a = 5;
-        int b = 5;
+        
         
         
         
