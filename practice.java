34065bcce03c981f231a235e3c8b66c55eaede1c
UNDISPUTED <devverma@Devs-MacBook-Air.local>
2026-08-22 18:10:17 +0530

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 public class practice{
     public static void main(String[] args){
-        // hi whos there...
+        // dont touch anything.
         int A = 5;
         System.out.println(A);
         
