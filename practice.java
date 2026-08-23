bf762245f53764ec5c055434121f8f3383de6bcb
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 05:23:32 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,23 +2,24 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter A: ");
-        int A = sc.nextInt();
-        System.out.print("Enter B: ");
-        int B = sc.nextInt();
-        System.out.print("Enter any operator( +, -, *, / ): ");
-        char operator = sc.next().charAt(0);
+        
+        // System.out.print("Enter A: ");
+        // int A = sc.nextInt();
+        // System.out.print("Enter B: ");
+        // int B = sc.nextInt();
+        // System.out.print("Enter any operator( +, -, *, / ): ");
+        // char operator = sc.next().charAt(0);
 
-        switch(operator){
-            case '+' : System.out.println("Addition is: "+(A+B));
-                       break;
-            case '-' : System.out.println("Subtraction is: "+(A-B));
-                       break;
-            case '*' : System.out.println("Multiplication is: "+(A*B));
-                       break;
-            case '/' : System.out.println("Division is: "+(A/B));
-                       break;
-            default : System.out.println("You entered a wrong operator");
+        // switch(operator){
+        //     case '+' : System.out.println("Addition is: "+(A+B));
+        //                break;
+        //     case '-' : System.out.println("Subtraction is: "+(A-B));
+        //                break;
+        //     case '*' : System.out.println("Multiplication is: "+(A*B));
+        //                break;
+        //     case '/' : System.out.println("Division is: "+(A/B));
+        //                break;
+        //     default : System.out.println("You entered a wrong operator");
             sc.close();
         }
 
