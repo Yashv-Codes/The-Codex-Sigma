520747a298fa1f1641db4174116ff6c0899d378e
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:40:38 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
-import java.util.*;
+// import java.util.*;
 public class practice{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         double num = sc.nextDouble();
 
