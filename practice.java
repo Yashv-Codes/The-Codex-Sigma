8abe45c6f7a8929162b5dabb0c5cde93af8d7548
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:43:08 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,17 +1,4 @@
-// import java.util.*;
-public class practice{
-    public static void main(String[] args){
-        // Scanner sc = new Scanner(System.in);
-        double temp = 103.5;
-        if(temp > 100)
-            System.out.println("You have fever");
-        else{
-            System.out.println("You don't have fever");
-        }
-        
-        // sc.close();
-        }
-    }
+
 
 
 
