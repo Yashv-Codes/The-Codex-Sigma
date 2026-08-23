73dc2f68d6cea990941b6c8cffa7c91f6add8cd3
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:45:43 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,6 +3,7 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number (1-7): ");
+        int weekday
 
     }
 }
