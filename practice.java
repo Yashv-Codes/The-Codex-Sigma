788b56cc2008b8b5edacc892e5f0a4f0a6fc9a62
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 05:14:54 +0000

    DSA

--- practice.java
+++ practice.java
@@ -18,7 +18,7 @@ public class practice{
                        break;
             case '/' : System.out.println("Division is: "+(A/B));
                        break;
-            default : System.out.println("")
+            default : System.out.println("You entered a wrong operator");
 
 
         }
