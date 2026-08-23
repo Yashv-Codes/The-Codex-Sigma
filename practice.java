ea4173808d411543f964595a4d42b141bcd2a2d0
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 05:29:47 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,31 +1,7 @@
 import java.util.*;
 public class practice{
     public static void main(String[] args){
-        // Scanner sc = new Scanner(System.in);
-        // long a = 5l;
-        // float b = a;
-        // System.out.println(b);
-        // // System.out.print("Enter A: ");
-        // // int A = sc.nextInt();
-        // // System.out.print("Enter B: ");
-        // // int B = sc.nextInt();
-        // // System.out.print("Enter any operator( +, -, *, / ): ");
-        // // char operator = sc.next().charAt(0);
-
-        // // switch(operator){
-        // //     case '+' : System.out.println("Addition is: "+(A+B));
-        // //                break;
-        // //     case '-' : System.out.println("Subtraction is: "+(A-B));
-        // //                break;
-        // //     case '*' : System.out.println("Multiplication is: "+(A*B));
-        // //                break;
-        // //     case '/' : System.out.println("Division is: "+(A/B));
-        // //                break;
-        // //     default : System.out.println("You entered a wrong operator");
-        //     sc.close();
-        long a = 5L;
-        float b = a;
-        System.out.println(b);
+        
         }
     }
 
