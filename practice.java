6e00bece0bacd8d4065dd265dda1938f42073da9
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:40:48 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,15 +2,9 @@
 public class practice{
     public static void main(String[] args){
         // Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a no: ");
-        double num = sc.nextDouble();
-
-        if(num < 0)
-            System.out.println("It's a -ve num");
-        else{
-            System.out.println("It's a +ve num");
-        }
-        sc.close();
+        
+        
+        // sc.close();
         }
     }
 
