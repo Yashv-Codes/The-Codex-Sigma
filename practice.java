8ac6966c06b3e1cd46fdbcb9cc33ab04900f1b9a
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:50:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,30 +2,11 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a digit (1-7): ");
-        int weekday = sc.nextInt();
         
-        switch(weekday){
-            case 1 : System.out.println("Monday");
-                     break;
-            case 2 : System.out.println("Tuesday");
-                     break;
-            case 3 : System.out.println("Wednesday");
-                     break;
-            case 4 : System.out.println("Thrusday");
-                     break;
-            case 5 : System.out.println("Friday");
-                     break;
-            case 6 : System.out.println("Saturday");
-                     break;
-            case 7 : System.out.println("Sunday");
-                     break;
-            default: System.out.println("You entered a wrong digit");
-            sc.close();
         }
 
     }
-}
+
 
 
 
