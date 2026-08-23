515b32528a7f6d7c1f461cafd5acccd6c10ef353
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:53:13 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,10 +2,7 @@
 public class practice{
     public static void main(String[] args){
         // Scanner sc = new Scanner(System.in);
-        int a = 63, b = 36;
-        boolean x = (a<b)? true : false;
-        int y  = (a>b)? a : b;
-        System.out.println(x + " , " + y);
+        
         
         }
 
