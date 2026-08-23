aa955aa9a9c7281722ac9d0aed1535ab6555a177
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:41:21 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice{
     public static void main(String[] args){
         // Scanner sc = new Scanner(System.in);
         double temp = 103.5;
-        
+        if(temp > 100)
         
         // sc.close();
         }
