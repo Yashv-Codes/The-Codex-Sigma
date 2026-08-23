c18cfda8fa0480b1aecc4e8c72001d5457bc1254
Yashv-Codes <yashv5529@gmail.com>
2026-08-23 06:49:39 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number (1-7): ");
+        System.out.print("Enter a digit (1-7): ");
         int weekday = sc.nextInt();
         
         switch(weekday){
