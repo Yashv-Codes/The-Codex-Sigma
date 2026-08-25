d43baafbb13cf6ea4654bd80d13ea8058b38f349
Yashv-Codes <yashv5529@gmail.com>
2026-08-25 13:41:16 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,11 +1,7 @@
 // import java.util.*;
 public class practice{
     public static void main(String[] args){
-        int i = 0;
-        while(i<100){
-            System.out.println("hello");
-            i++;
-        }
+        
 
     }
 }
