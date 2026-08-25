c11e972a81002e8ad31a395dbde6bed90cf2cfe8
Yashv-Codes <yashv5529@gmail.com>
2026-08-25 13:05:05 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,15 +5,9 @@ public class practice{
         System.out.print("Enter a year: ");
         int year = sc.nextInt();
 
-        if(year % 4 == 0)
+        if(year % 4 == 0){}
             System.out.println("Regular Leap year");
-        else if(year % 100 == 0)
-            System.out.println("Not a Leap year");
-        else if(year % 400 == 0)
-            System.out.println("Century Leap year");
-        else{
-            System.out.println("Not a Leap year");
-        }
+        
         sc.close();
     }
     
