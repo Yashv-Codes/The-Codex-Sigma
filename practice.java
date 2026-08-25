1887949b94c121a4f16546e164948e559feea3ab
Yashv-Codes <yashv5529@gmail.com>
2026-08-25 13:43:24 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice{
     public static void main(String[] args){
         int i = 1;
         while(i <= 10){
-            System.out.println(i+ " ");
+            System.out.print(i+ " ");
             i++;
         }
 
