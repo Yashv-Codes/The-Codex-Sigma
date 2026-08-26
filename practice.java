4ca3bde937f816287a8e74facd98135c4451ce5e
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 13:54:14 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,8 @@ public class practice{
     public static void main(String[] args){
     int counter = 1;
     do{
-        System.out.println("Hi")
+        System.out.println("Hi");
+        counter++;
     }
     
         
