356d2b3fabd8ac4f4a025608e27d37e34703b843
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 13:59:31 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class practice{
     public static void main(String[] args){
-        for(int i=0; i<=5; i++){
-            if(i == 3)
-                break;
-            System.out.print(i+" ");
-        }
+        // for(int i=0; i<=5; i++){
+        //     if(i == 3)
+        //         break;
+        //     System.out.print(i+" ");
+        // }
         System.out.println();
 
     }
