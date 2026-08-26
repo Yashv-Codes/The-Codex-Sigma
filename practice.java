41d6a9da6cea27a2bef172de13f34f6e547a6a33
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 13:53:42 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,14 +1,7 @@
 import java.util.*;
 public class practice{
     public static void main(String[] args){
-    int n = 10899;
-    while(n!=0){
-        int lastdigit = n%10;
-        System.out.print(lastdigit+ " ");
-        n /= 10;
-
-    }
-    System.out.println();
+    
     
         
         
