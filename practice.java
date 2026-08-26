48c0a2a39f0e4abca714a509c3ae41a288d3e086
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 15:04:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int i = 1;
+        int i = 0;
         do{
             i++;
             if(i==4){
