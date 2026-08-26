ddf0f4458c2e5630a2c31fd82c4b1ae73e3b7c6e
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 13:18:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,11 +3,11 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
-        int range = sc.nextInt();
+        int n = sc.nextInt();
         int i = 1;
         int sum = 0;
         while(i <= range){
-            
+            sum += ran
         }
         
 
