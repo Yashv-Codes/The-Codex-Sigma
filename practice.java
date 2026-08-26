0e2f7c2cdb196a8dde8c44cf6f9fca9c9bd5f019
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 14:06:10 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,12 +3,12 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        // while(num != 0){
-        //     System.out.print("Enter a no: ");
-        //     if(10 % num == 0)
-        //         break;
-        //     System.out.println(num);
-        // }
+        while(num != 0){
+            System.out.print("Enter a no: ");
+            if(10 % num == 0)
+                break;
+            System.out.println(num);
+        }
 
     }
 }
