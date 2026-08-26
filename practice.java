c30aa7deb8a1f05ebc9d702083e4e0aa7ea9a514
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 15:22:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,7 +7,7 @@ public class practice{
             int n = sc.nextInt();
             if(n % 10 == 0)
                 continue;
-            System.out.print(n+)
+            System.out.println(n);
         }while(true);
         
     }
