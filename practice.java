7c1747cb12c9f165e7805338ee2a8028a9fded54
Yashv-Codes <yashv5529@gmail.com>
2026-08-26 13:27:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,10 +1,9 @@
 import java.util.*;
 public class practice{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a no: ");
-        int n = sc.nextInt();
-        for(int i=1; i<=n; i++){
+       
+        
+        for(int i=1; i<=4; i++){
             System.out.print(i+" ");
         }
         System.out.println();
