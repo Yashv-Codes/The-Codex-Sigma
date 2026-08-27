640a97338aec1846d0a8df4b5a8313027bb82914
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 13:34:35 +0000

    DSA

--- practice.java
+++ practice.java
@@ -11,7 +11,10 @@ public class practice{
         }
         else{
             for(int i=2; i<n; i++){
-                if(n % i == 0)
+                if(n % i == 0){
+                    System.out.println("Not prime");
+                    return;
+                }
             }
         }
     }
