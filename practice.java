41676d3f7d6fd3c7349dfba7d2dab89b9e27dedf
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:11:50 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,15 +1,7 @@
 import java.util.*;
 public class practice{
-    public static int table(int n, int fact){
-        if(n < 0)
-            return -1;
-        else if(n == 0 || n == 1)
-            return 1;
-        for(int i=2; i<=n; i++){
-            fact *= i;
-        }
-        return fact;
-
+    public static int table(int n){
+        
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
