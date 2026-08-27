4bbb0ab362a58979d86e5ad95c56a19f252d622c
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:12:20 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class practice{
     public static int table(int n){
-        
+        for(int i=1; i<=n; i++){
+            
+        }
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
