4739e8487c03e874e1a6bf86d0708334c613bf91
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 14:47:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
+        int sum_even = 0;
+        int odd_sum = 0;
+        for(int i=1; i<=20)
     
         
         
