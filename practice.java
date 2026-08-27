455c0679cba6255ada6f44f78e0416b884081392
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 14:59:06 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,17 +2,7 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int sum_even = 0;
-        int odd_sum = 0;
-        for(int i=1; i<=20; i++){
-            if(i % 2 == 0)
-                sum_even += i;
-            else{
-                odd_sum += i;
-            }
-        }
-        System.out.println("Sum of even integers are: "+sum_even);
-        System.out.println("Sum of odd integers are: "+odd_sum);
+        
     }
 }
 
