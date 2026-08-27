31350f54876404d35c1914e8322bb0d22fe5fb79
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:03:15 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,18 +1,15 @@
 import java.util.*;
 public class practice{
     public static int factorial(int n, int fact){
-        
+        if(n < 0)
+
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
         int fact = 1;
-        if(n < 0)
-            return;
-        else if(n == 0 || n == 1)
-            return 1;
-
+        
     }
 }
 
