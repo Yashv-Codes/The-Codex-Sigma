8ef08c8e895bd21b43c68bf300700767007067a2
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 14:47:25 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,22 +2,7 @@ import java.util.*;
 public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a no: ");
-        int n = sc.nextInt();
-
-        if(n <= 1){
-            System.out.println("Not prime");
-            return;
-        }
-        else{
-            for(int i=2; i<n; i++){
-                if(n % i == 0){
-                    System.out.println("Not prime");
-                    return;
-                }
-            }
-            System.out.println("Prime no");
-        }
+        
     
         
         
