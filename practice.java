485877facea35b6d597fc682ee0f89ba33207835
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 14:59:38 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,8 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
-        int 
+        int n = sc.nextInt();
+        int factorial = 1;
     }
 }
 
