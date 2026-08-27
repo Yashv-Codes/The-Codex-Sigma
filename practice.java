b86b247cbbf7c4275649e8a13b019c73d434d3c4
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:05:03 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,6 +5,9 @@ public class practice{
             return -1;
         else if(n == 0 || n == 1)
             return 1;
+        for(int i=2; i<=n; i++){
+            
+        }
 
     }
     public static void main(String[] args){
