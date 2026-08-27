e9a9ff6557422b7c64912b5241bbd99c3c169188
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:02:41 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class practice{
-    public static int
+    public static int factorial(int n)
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
