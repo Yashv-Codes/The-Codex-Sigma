2d6e5b2d03b4127a05749c324bfa2f2443d9cda1
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 15:16:43 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice{
     public static void table(int n){
         for(int i=1; i<=10; i++){
-            System.out.println(""+n*i);
+            System.out.println(""+(n*i));
             
         }
     }
