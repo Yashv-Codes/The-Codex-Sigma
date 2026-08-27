22d9fb373aae9752f46c933b2d25ddc01128be5d
Yashv-Codes <yashv5529@gmail.com>
2026-08-27 14:48:15 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,7 +5,7 @@ public class practice{
         int sum_even = 0;
         int odd_sum = 0;
         for(int i=1; i<=20; i++){
-            if(i )
+            if(i % 2 == 0)
         }
     
         
