0264d2aa1eb86db31ef0281d5414296e184dd1e8
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:04:06 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice{
         int n = sc.nextInt();
         char ch = 'A';
         for(int line=1; line<=n; line++){
-            for(int character=1; <=line; ++){
+            for(int character=1; character<=line; character++){
                 System.out.print(ch+" ");
                 ch++;
             }
