18343c49fc5dc6837257320fe6078a3afbe6ef35
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 08:32:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,19 +1,7 @@
 import java.util.*;
 public class practice{
     public static void table(int n){
-        for(int i=1; i<=10; i++){
-            System.out.println(n + " X " + i + " = " +(n*i));
-            
-        }
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a no: ");
-        int n = sc.nextInt();
-        table(n);
         
-        
-        sc.close();
         
     }
 }
