68e2cc831c6ec704cfb8bc116d3f18fcfd5453db
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:52:22 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,7 +5,7 @@ public class practice{
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
         for(int line=1; line<=n; line++){
-            for(int num=1; )
+            for(int digit)
         }
         
         sc.close();
