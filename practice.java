b50fbe6da35060b272a532160632392e84ce7b16
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 08:43:12 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class practice{
     public static void table(int n){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a no: ");
+        int n = sc.nextInt();
         
     }
 }
