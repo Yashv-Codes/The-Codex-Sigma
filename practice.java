6b928967f3188c12f54cd7d770faccd482ab9504
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 10:14:08 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,11 +4,11 @@ public class practice{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
-        int num = 1;
+        
         for(int line=1; line<=n; line++){
             for(int digit=1; digit<=line; digit++){
                 System.out.print(num+" ");
-                num++;
+                
             }
             System.out.println();
         }
