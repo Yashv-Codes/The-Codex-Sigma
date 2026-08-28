66dbf80ca0d3095ba543970530b642bc0954ab67
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:27:04 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,9 @@ public class practice{
         int n = sc.nextInt();
         
         for(int line=1; line<=n; line++){
-            for(int space=1; space<=n-line; space++)
+            for(int space=1; space<=n-line; space++){
+                System.out.print
+            }
             for(int star=1; star<=n+1; star++){
                 
                 
