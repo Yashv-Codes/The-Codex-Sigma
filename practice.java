78af5f3f99985a2a9225beb2608f406d6233b6c6
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:53:50 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,7 +7,7 @@ public class practice{
         int num = 
         for(int line=1; line<=n; line++){
             for(int digit=1; digit<=line; digit++){
-                
+                System.out.print(num+" ");
             }
         }
         
