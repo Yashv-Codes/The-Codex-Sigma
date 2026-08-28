1577ae10bf7bfebce22fc2c05a28319c73ca19e5
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:11:31 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,7 +7,7 @@ public class practice{
         
         for(int line=1; line<=n; line++){
             for(int star=1; star<=5; star++){
-                if(line==1)
+                if(line==1 || line==n ||)
                 System.out.print("*"+" ");
                 
             }
