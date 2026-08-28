9b06f6835937ff7b5620bb50f057031ba38f6073
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:40:39 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,9 +6,7 @@ public class practice{
         int n = sc.nextInt();
         
         for(int line=1; line<=n; line++){
-            for(int space=1; space<=n-line; space++){
-                System.out.print(" "+" ");
-            }
+            
             for(int star=1; star<=line; star++){
                 System.out.print("*"+" ");
             }
