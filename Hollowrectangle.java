9220d72cc1614b7e0526ddda5b94a5d40043f0f5
Yashv-Codes <yashv5529@gmail.com>
2026-08-28 09:16:14 +0000

    DSA

--- Hollowrectangle.java
+++ Hollowrectangle.java
@@ -2,8 +2,8 @@ public class Hollowrectangle{
     public static void main(String args[]){
         int n = 4;
         for(int line=1; line<=n; line++){
-            for(int stars=1; stars<=5; stars++){
-                if( line==1 || line==n || stars==1 || stars==5)
+            for(int stars=1; stars<=n+1; stars++){
+                if( line==1 || line==n || stars==1 || stars==n+1)
                     System.out.print("*"+" ");
                 else
                     System.out.print(" "+" ");
