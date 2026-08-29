fbf85f1a5e656f97eac63ea6f9aad7a7097ce2b8
Yashv-Codes <yashv5529@gmail.com>
2026-08-29 10:57:15 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class practice{
-    public static void hollow_rhombus(int n){
+    public static void diamond(int n){
         for(int line=1; line<=n; line++){
             for(int space=1; space<=n-line; space++){
                 System.out.print("  ");
