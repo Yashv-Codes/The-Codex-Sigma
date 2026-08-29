b467d886c3b161221ce658710991234e69307e93
Yashv-Codes <yashv5529@gmail.com>
2026-08-29 10:15:21 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice{
     public static void butterfly(int n){
         // 1st half
         for(int line=1; line<=n; line++){
-            
+            for(int star=1; star<=line; star++)
         }
     }
     public static void main(String[] args){
