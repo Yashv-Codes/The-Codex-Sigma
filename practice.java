b89fdc7a6470283a0238b4e0c8c20953c2b59c7a
Yashv-Codes <yashv5529@gmail.com>
2026-08-29 10:25:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,14 +1,14 @@
 import java.util.*;
 public class practice{
     public static void butterfly(int n){
-        
+
     }
         
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
         int n = sc.nextInt();
-        butterfly(n);
+        
         sc.close();
         
     }
