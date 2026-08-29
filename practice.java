0f940057475c63f3e4f235e124d9c65c5a9ecf2e
Yashv-Codes <yashv5529@gmail.com>
2026-08-29 10:24:52 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,34 +1,9 @@
 import java.util.*;
 public class practice{
     public static void butterfly(int n){
-        // 1st half
-        for(int line=1; line<=n; line++){
-            for(int star=1; star<=line; star++){
-                System.out.print("* ");
-            }
-            for(int space=1; space<=2*(n-line); space++){
-                System.out.print("  ");
-            }
-            for(int star=1; star<=line; star++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        // 2nd Half
-        for(int line=n; line>=1; line--){
-            for(int star=1; star<=line; star++){
-                System.out.print("* ");
-            }
-            for(int space=1; space<=2*(n-line); space++){
-                System.out.print("  ");
-            }
-            for(int star=1; star<=line; star++){
-                System.out.print("* ");
-            }
-            System.out.println();
-
-        }
+        
     }
+        
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a no: ");
