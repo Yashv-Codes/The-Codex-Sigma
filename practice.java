bae465c3bb65167fa89d3face29b091f50febd59
Yashv-Codes <yashv5529@gmail.com>
2026-08-29 10:27:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class practice{
     public static void solid_rhombus(int n){
-        for(int line=1; line<)
+        for(int line=1; line<=n; line++){
+            for(int space=1; space<=n; space++)
+        }
 
     }
         
