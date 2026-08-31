7cf4de2b80e376181c201c3c2b2c98e3f9698567
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 13:52:16 +0000

    DSA

--- practice.java
+++ practice.java
@@ -11,8 +11,7 @@ public class practice{
         
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        
         System.out.println("product = ");
         
        
