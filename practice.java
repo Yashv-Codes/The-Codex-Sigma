a8cfddebcaa80b7bc86f3fc5eee048404fe957a4
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 13:51:38 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,13 +2,14 @@ import java.util.*;
 public class practice{
     public static int factorial(int n){
         
+        
     }
         
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("product = "+product(a,b));
+        System.out.println("product = ");
         
        
         sc.close();
