b8d81c15c8cd2330dfc1eecd7deac2922f609fee
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 15:18:28 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,7 +5,7 @@ public class practice{
         return a+b;
     }
     public static int sum(float a, float b){
-        return (float)(a+b);
+        return (int)a+b;
     }
         
     public static void main(String[] args){
