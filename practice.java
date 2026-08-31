d889e4ccc9dd38051a1a5b66d109e47463353015
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 16:09:15 +0000

    DSA

--- practice.java
+++ practice.java
@@ -8,7 +8,7 @@ public class practice{
         System.out.println();
     }
     public static boolean isPrime(int n){
-        if(n <= 0 || n == 1)
+        if(n <= 1)
             return false;
         for(int i=2; i<=Math.sqrt(n); i++){
             if(n % i == 0)
