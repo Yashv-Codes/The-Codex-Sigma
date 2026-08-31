58a5bdaeec4fdf8bd2112136b2d33c6e257a5817
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 15:59:37 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,17 +1,11 @@
 import java.util.*;
 public class practice{
     
-    public static int sum(int a , int b){
-        return a+b;
-    }
-    public static float sum(float a, float b){
-        return a+b;
-    }
+    
         
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println(sum(5,3));
-        System.out.println(sum(5.0f,3.0f));
+       
         
         
         
