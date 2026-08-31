2a398351e86d4cb231a5f08fb6568b15e6d09735
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 13:52:01 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,11 @@
 import java.util.*;
 public class practice{
     public static int factorial(int n){
-        
+        int fact = 1;
+        for(int i=2; i<=n; i++){
+            fact *= i;
+        }
+        return fact;
         
     }
         
