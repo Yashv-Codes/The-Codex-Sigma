c59a24d58fc755883d9c5d1afb03ff004d7258c9
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 16:02:41 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class practice{
     public static printPrimesinRange(int n){
-        for()
+        for(int i=0; i)
     }
     public static boolean isPrime(int n){
         if(n <= 0 || n == 1)
