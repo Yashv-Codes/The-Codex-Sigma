faa78070e6161b5f99920b6d5c80f3ad0fb2d2cc
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 16:11:17 +0000

    DSA

--- practice.java
+++ practice.java
@@ -17,8 +17,6 @@ public class practice{
         return true;
     }
     
-    
-        
     public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
@@ -27,6 +25,7 @@ public class practice{
         
     }
 }
+
 
 
     
