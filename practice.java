5b39d7e3a8b574cd5115a501ceac9c449e5c49cf
Yashv-Codes <yashv5529@gmail.com>
2026-08-31 16:03:42 +0000

    DSA

--- practice.java
+++ practice.java
@@ -20,7 +20,7 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
        int n = sc.nextInt();
-       System.out.println()
+       System.out.println(printPrimesinRange())
         
         
         
