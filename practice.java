5506cc4f153d35d246d53fc803d8d782b35df2d0
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 12:55:28 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,8 +2,8 @@ public class practice {
     public static int BintoDec(int binary){
         int decimal = 0;
         int pow = 0;
-        int lastdigit = binary%10;
         while(binary!=0){
+            int lastdigit = binary%10;
             decimal += (lastdigit*Math.pow(2,pow));
             binary /= 10;
             pow++;
