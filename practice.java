82fbc9f350c57fc7a0f19abedca927cb8db1c1d5
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:06:34 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 public class practice {
     public static int averageofnum(int a, int b, int c){
-        return (a+b+b)/2;
+        return (a+b+c)/3;
     }
     public static void main(String[] args){
         System.out.println("Avg = "+averageofnum(45,32,11));
