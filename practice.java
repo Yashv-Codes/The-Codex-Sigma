fd192427c0014920bfaf508c2f868622160a3f8a
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:08:05 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,10 +1,7 @@
 public class practice {
-    public static float averageofnum(float a, float b, float c){
-        return (a+b+c)/3;
-    }
+    
     public static void main(String[] args){
-        System.out.println("Avg = "+averageofnum(45f,32f,11f));
-        
+       
     }
     
 }
