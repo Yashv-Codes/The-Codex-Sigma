f2a2f3d5ccf74cf013ccc31712abc5a1a5a32685
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:35:56 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,6 +2,9 @@ public class practice {
     public static int sumofdigit(int digit){
         int sum = 0;
         digit = Math.abs(digit);
+        while(n != 0){
+            
+        }
     }
     
     public static void main(String[] args){
