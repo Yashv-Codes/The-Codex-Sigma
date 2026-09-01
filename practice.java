cd8441a3d1786026bbd0a89d15148c96ac4bd8ec
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:09:24 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,8 @@
 public class practice {
     public static boolean isEven(int n){
-        if(n )
+        if(n % 2 == 0)
+            return true;
+        return false;
     }
     
     public static void main(String[] args){
