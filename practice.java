a1a0369c835ebffbec1c6e96c05c488637c295ab
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:28:56 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,6 +3,8 @@ public class practice {
         if(n < 0 || (n % 10 == 0 && n != 0))
             return false;
         int palindrome = n;
+        int rev = 0;
+        
     }
     
     public static void main(String[] args){
