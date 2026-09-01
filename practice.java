84a57a3ac638ed43082744d0899b8059cf323839
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:05:37 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,5 +1,7 @@
 public class practice {
-    public static int averageofnum(int a, int b, int c)
+    public static int averageofnum(int a, int b, int c){
+        return (a+b+b)/2;
+    }
     public static void main(String[] args){
         
     }
