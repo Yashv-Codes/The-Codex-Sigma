1c17fe2633243d3166dcd0773db01a671589d7fb
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:30:30 +0000

    DSA

--- practice.java
+++ practice.java
@@ -13,7 +13,7 @@ public class practice {
     }
     
     public static void main(String[] args){
-        System.out.println(());
+        System.out.println(isPalindrome(3456));
        
     }
     
