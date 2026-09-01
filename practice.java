0ac87985bd31530aa192e671741cb1546b6ba631
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 12:53:04 +0000

    DSA

--- practice.java
+++ practice.java
@@ -11,7 +11,7 @@ public class practice {
         return decimal;
     }
     public static void main(String[] args){
-        System.out.println("Decimal num")
+        System.out.println("Decimal = "+)
     }
     
 }
