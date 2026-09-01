bafd6d4f032d9766cadbb3f7349edda6ea30e947
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:29:54 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,8 @@ public class practice {
         int rev = 0;
         while(n != 0){
             int lastdigit = n % 10;
-            rev = (rev*10
+            rev = (rev*10)+lastdigit;
+            n /= 10;
         }
     }
     
