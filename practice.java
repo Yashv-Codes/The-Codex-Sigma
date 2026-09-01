ee8a76395b4bd5a116619d5e6df977200b549af9
Yashv-Codes <yashv5529@gmail.com>
2026-09-01 14:28:10 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,6 +2,7 @@ public class practice {
     public static boolean isPalindrome(int n){
         if(n < 0 || (n % 10 == 0 && n != 0))
             return false;
+        int 
     }
     
     public static void main(String[] args){
