04446c4cd3494163bd1d038a0ea648cd5b137f36
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:07:21 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,7 +5,7 @@ public class practice{
     public static void main(String[] args){
         int a = 50;
         int b = 67;
-        System.out.println(Math.sqrt(a,b));
+        System.out.println(Math.sqrt(a));
        
     }
 }
