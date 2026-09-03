3d9578b605300807ef079cf2ffb05bfb9758d6f3
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 13:34:46 +0000

    DSA

--- practice.java
+++ practice.java
@@ -14,7 +14,7 @@ public class practice{
         Scanner sc = new Scanner(System.in);
         // int nums[] = {2, 4, 6, 8, 10, 12, 14};
         String menu[] = {"noodles", "pav bhaji", "idli sambhar", "samosa", "butter paneer"};
-        String key = "samosa";
+        String key = "puchka";
         int index = linear_search(menu,key);
         if(index == -1)
             System.out.println("Key not found");
