c89a921b4cbafad7d29ccca3d2766dde29dd065a
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:59:20 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int marks[] = new int[n];
-        System.out.println("Length of array = "+marks.length);
+        int n = sc.nextInt();
         sc.close();
         
         
