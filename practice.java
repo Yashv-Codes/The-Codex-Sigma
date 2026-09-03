031faae2118cfbf6bb3d9aad9c7e0ef8eb36da9a
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:30:13 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,9 +3,7 @@ public class practice{
     
     
     public static void main(String[] args){
-        int a = 5;
-        int b = 6;
-        System.out.println(Math.pow(a,b));
+        int arr[] = new int[10];
        
     }
 }
