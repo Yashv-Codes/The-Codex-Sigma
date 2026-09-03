5aa3931ab9c73f75ba9a75d5ccb0572b4995c2e2
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:44:21 +0000

    DSA

--- practice.java
+++ practice.java
@@ -9,7 +9,9 @@ public class practice{
         marks[0] = sc.nextInt();
         marks[1] = sc.nextInt();
         marks[2] = sc.nextInt();
-        System.out.println("Physics = "+marks[0])
+        System.out.println("Physics = "+marks[0]);
+        System.out.println("Chemistry = "+marks[1]);
+        System.out.println("Maths = "+marks[2]);
         
         
        
