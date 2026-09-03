72c402104cc940cc7988aeeaed589b064a3b6b63
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:30:35 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,7 +4,8 @@ public class practice{
     
     public static void main(String[] args){
         int arr[] = new int[10];
-        int arr[] = {1,2,3};
+        int num[] = {1,2,3};
+        int
        
     }
 }
