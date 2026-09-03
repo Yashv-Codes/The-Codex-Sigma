344bea8a72ace53ab9f3ac747c303c75e3e74762
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 13:21:01 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,10 +1,11 @@
 import java.util.*;
 public class practice{
-    
+
    
     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int nums[] = new int[]
         
         sc.close();
     }
