cba50d1e33440ce27c64d8a6b01cac5d1b2fe5e1
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:50:23 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,16 +6,7 @@ public class practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int marks[] = new int[10];
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
-        System.out.println("Physics = "+marks[0]);
-        System.out.println("Chemistry = "+marks[1]);
-        System.out.println("Maths = "+marks[2]);
-        // marks[2] = marks[2] + 1;
-        // System.out.println("Maths = "+marks[2]);
-        float percentage = (marks[0]+marks[1]+marks[2])/3;
-        System.out.println("Percentage = "+percentage);
+        
         sc.close();
         
         
