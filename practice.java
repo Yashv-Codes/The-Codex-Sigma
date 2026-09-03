d0a1e62292f5f915f27f5c05f8f72e2b0fccd248
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 13:28:46 +0000

    DSA

--- practice.java
+++ practice.java
@@ -15,6 +15,7 @@ public class practice{
         // int nums[] = {2, 4, 6, 8, 10, 12, 14};
         String menu[] = {"noodles", "pav bhaji", "idli sambhar", "samosa", "butter paneer"};
         String key = "samosa";
+        if(key == -1)
         System.out.println(linear_search(menu,key));
         
         sc.close();
