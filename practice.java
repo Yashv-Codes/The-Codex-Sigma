557e2af3411784288bdfb745aacc5dc99eabad4f
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 13:03:12 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice{
     public static void update(int marks[]){
         for(int i=0; i<marks.length; i++){
-            marks[i]
+            marks[i] = marks[i]+1;
         }
     }
 
