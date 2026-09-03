691ffcd848d6ad3683538f37753863fa5750fa6a
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 13:20:36 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,26 +1,10 @@
 import java.util.*;
 public class practice{
-    public static void update(int marks[]){
-        for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i]+1;
-        }
-    }
+   
     
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int marks[] = new int[n];
-        // taking input
-        for(int i=0; i<marks.length; i++){
-            marks[i] = sc.nextInt();
-        }
-        System.out.println();
-        update(marks);
-        // print array
-        for(int i=0; i<marks.length; i++){
-            System.out.print(marks[i]+" ");
-        }
-        System.out.println();
+        
         sc.close();
     }
 }
