9488ec10fa53ca76f3c55931d32fdac13f737140
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 05:24:06 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,8 +3,9 @@ public class practice{
     
     
     public static void main(String[] args){
-        System.out.println("Sum of digits = "+sumofdigit(-6778));
+        
        
     }
+}
     
 
