330cf176bf96c2ab6394cdb6a6bf5a34624f2e82
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 05:23:22 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,12 +1,6 @@
 public class practice {
     public static int sumofdigit(int digit){
-        int sum = 0;
-        digit = Math.abs(digit);
-        while(digit != 0){
-            sum += digit % 10;
-            digit /= 10;
-        }
-        return sum;
+        
     }
     
     public static void main(String[] args){
