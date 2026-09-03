1c2aa2e41852875082944dc8a316e633f64a151d
Yashv-Codes <yashv5529@gmail.com>
2026-09-03 12:31:24 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice{
         int arr[] = new int[10];
         int num[] = {1,2,3};
         int morenum[] = {4,5,6};
-        String fruits = {"mango", "guava", "lichi"};
+        String fruits[] = {"mango", "guava", "lichi"};
        
     }
 }
