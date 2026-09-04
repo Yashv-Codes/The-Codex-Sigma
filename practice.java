ea6bad29b3f7a9807fbf960b349634e125db03c0
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 13:01:55 +0000

    DSA

--- practice.java
+++ practice.java
@@ -27,11 +27,11 @@ public class practice{
         int count = 0;
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
-                System.out.print(nums[i]+","+nums[j]+" ");
+                System.out.print("("nums[i]+","+nums[j]+" ");
                 count++;
             }
             System.out.println();
-            System.out.println
+            System.out.println("Total pairs = "+count);
         }
 
     }
