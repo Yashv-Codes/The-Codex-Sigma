2eb308a5fa168f8c1e151106ad6adad16ed4941c
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 07:06:31 +0000

    DSA

--- practice.java
+++ practice.java
@@ -36,12 +36,7 @@ public class practice{
     
     public static void main(String[] args){
         int nums[] = {2, 4, 6, 8, 10};
-        int key = 11;
-        reverse(nums);
-        for(int i=0; i<nums.length; i++){
-            System.out.print(nums[i]+" ");
-        }
-        System.out.println();
+        
         
         
     }
