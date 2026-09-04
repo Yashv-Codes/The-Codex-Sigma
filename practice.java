f953330938c11e41f507ba6db7c6f82935c0225f
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 05:58:14 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,7 +4,7 @@ public class practice{
     
     public static void main(String[] args){
         int si= 0;
-        int end = nums.length-1;
+        int ei = nums.length-1;
         int mid = si+(ei-si)/2;
     }
 }
