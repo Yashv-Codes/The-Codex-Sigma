43d2d9ef4189f8d70ac70a5e484c81e51a2cd697
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 06:46:00 +0000

    DSA

--- practice.java
+++ practice.java
@@ -30,7 +30,7 @@ public class practice{
         int key = 11;
         reverse(nums);
         for(int i=0; i<nums.length; i++){
-            
+            System.out.print()
         }
         
         
