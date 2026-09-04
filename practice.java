500441cbcce9ee608ae9ff6d1f4ef3cc1a437253
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 06:09:41 +0000

    DSA

--- practice.java
+++ practice.java
@@ -18,7 +18,7 @@ public class practice{
     
     public static void main(String[] args){
         int nums[] = {2, 4, 6, 8, 10, 12, 14};
-        int key = 10;
+        int key = 11;
         System.out.println(binary_search(nums, key));
         
         
