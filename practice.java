9daeca2e395547fdd906ff54587ec2e3743c3390
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 13:44:43 +0000

    DSA

--- practice.java
+++ practice.java
@@ -40,6 +40,7 @@ public class practice{
         int count = 0;
         for(int i=0; i<nums.length; i++){
             for(int j=i; j<nums.length; j++){
+                int sum = 0;
                 for(int k=i; k<=j; k++){
                     System.out.print(nums[k]+" ");
                     
