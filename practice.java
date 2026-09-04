a39059bfa652989935863a40f71bfbf2f50ca598
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 07:05:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -24,12 +24,13 @@ public class practice{
         }
     }
     public static void pairs(int nums[]){
+        
 
     }
     
     
     public static void main(String[] args){
-        int nums[] = {2,4, 6};
+        int nums[] = {2, 4, 6, 8, 10};
         int key = 11;
         reverse(nums);
         for(int i=0; i<nums.length; i++){
