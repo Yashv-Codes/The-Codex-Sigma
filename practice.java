2fb4bf77c4c8660f2d410069b313a1068a2ab097
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 13:01:16 +0000

    DSA

--- practice.java
+++ practice.java
@@ -24,6 +24,7 @@ public class practice{
         }
     }
     public static void pairs(int nums[]){
+        int count = 0;
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 System.out.print(nums[i]+","+nums[j]+" ");
