f41d6cf15592b666615aa9abde2ba7eb6953cdd9
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 05:59:10 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,6 +4,7 @@ public class practice{
     
     public static void main(String[] args){
         int nums[] = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
         int si= 0;
         int ei = nums.length-1;
         int mid = si+(ei-si)/2;
