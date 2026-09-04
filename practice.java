b2875ba5bf2eb920f27c5548e783f03b5a9ab303
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 13:31:17 +0000

    DSA

--- practice.java
+++ practice.java
@@ -39,7 +39,7 @@ public class practice{
     public static void subarrays(int nums[]){
         int count = 0;
         for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
+            for(int j=i; j<nums.length; j++){
                 for(int k=i; k<=j; k++){
                     System.out.print("("+nums[k]+")"+" ");
                     count++;
