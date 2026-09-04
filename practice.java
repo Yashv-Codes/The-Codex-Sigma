dc30765c7124c820cea984dcc61a2b20f6487af1
Yashv-Codes <yashv5529@gmail.com>
2026-09-04 13:55:20 +0000

    DSA

--- practice.java
+++ practice.java
@@ -37,7 +37,7 @@ public class practice{
 
     }
     public static void subarrays(int nums[]){
-        int lasrgest = Integer.MIN_VALUE;
+        int largest = Integer.MIN_VALUE;
         int smallest = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++){
             for(int j=i; j<nums.length; j++){
@@ -45,7 +45,7 @@ public class practice{
                 for(int k=i; k<=j; k++){
                     sum += nums[k];
                 }
-                
+
                 
                 
                 
