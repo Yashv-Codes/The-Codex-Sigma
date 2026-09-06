4833e536a7e1864f25c21fcb4527c7847d43e8e2
Yashv-Codes <yashv5529@gmail.com>
2026-09-06 06:31:01 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,6 +7,10 @@ public class practice{
         for(int i=0; i<prefix.length; i++){
             prefix[i] = prefix[i-1] + nums[i];
         }
+
+        for(int i=0; i<nums.length; i++){
+            for(int j)
+        }
         
         
     }
