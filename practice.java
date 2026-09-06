e5f128ebb2edba3b05697eeeab364b6a94002256
Yashv-Codes <yashv5529@gmail.com>
2026-09-06 06:35:04 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,7 +4,7 @@ public class practice{
         int currsum = 0, maxSum = Integer.MIN_VALUE;
         int prefix[] = new int[nums.length];
         prefix[0] = nums[0];
-        for(int i=0; i<prefix.length; i++){
+        for(int i=1; i<prefix.length; i++){
             prefix[i] = prefix[i-1] + nums[i];
         }
 
@@ -12,8 +12,7 @@ public class practice{
             for(int j=i; j<nums.length; j++){
                 currsum = i==0 ? prefix[j] : prefix[j] - prefix[i-1];
             }
-            if(currsum > maxSum)
-                maxSum = currsum;
+           
         }
         return maxSum;
         
