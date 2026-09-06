2763fe89828ac92a733d951daa7b13a6e3a36ddd
Yashv-Codes <yashv5529@gmail.com>
2026-09-06 06:31:18 +0000

    DSA

--- practice.java
+++ practice.java
@@ -9,7 +9,9 @@ public class practice{
         }
 
         for(int i=0; i<nums.length; i++){
-            for(int j)
+            for(int j=i; j<nums.length; j++){
+                currsum = 
+            }
         }
         
         
