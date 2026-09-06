6a56290849630d236ecd98a785413c935e410f66
Yashv-Codes <yashv5529@gmail.com>
2026-09-06 06:32:07 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,6 +12,7 @@ public class practice{
             for(int j=i; j<nums.length; j++){
                 currsum = i==0 ? prefix[j] : prefix[j] - prefix[i-1];
             }
+            if(currsum < maxSubarraysum(nums)
         }
         
         
