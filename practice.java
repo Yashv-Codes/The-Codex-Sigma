5ff4c67be522e49c26a7ae6a587dc961649c6427
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 13:03:44 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,6 +5,7 @@ public class practice{
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
                 if(nums[j] == nums[i])
+                    count++;
             }
         }
     }
