c4efc4bd6ce0fb5a59fd2343ece4c0d97deafab1
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 05:54:46 +0000

    DSA

--- practice.java
+++ practice.java
@@ -13,6 +13,9 @@ public class practice{
     public static void neg_loop(int nums[]){
         int smallest = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++){
+            if(nums[i] >= 0)
+                return;
+            if
             
                 
         }
