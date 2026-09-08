cb401e6c640e4010eded9a45529e8d7bb9cccce4
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 05:47:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,7 +12,9 @@ public class practice{
     }
     public static void neg_loop(int nums[]){
         int smallest = Integer.MAX_VALUE;
-        for(int i=0; i<nums.length; i++)
+        for(int i=0; i<nums.length; i++){
+            
+        }
     }
     
     public static void main(String[] args){
