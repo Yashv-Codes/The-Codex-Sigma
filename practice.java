fc70e98f3f19e5627f74d79e7252b2035fc80456
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 13:04:51 +0000

    DSA

--- practice.java
+++ practice.java
@@ -10,11 +10,13 @@ public class practice{
                     return true;
             }
         }
+        return false;
     }
     
     
     public static void main(String[] args){
     int nums[] = {1, 2, 3, 1};
+    System.out.println()
     
     }
 }
