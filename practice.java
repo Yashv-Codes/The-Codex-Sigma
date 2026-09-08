ad689968b1ca7be7f4fac72455d36e3387d3dcce
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 05:22:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,7 +12,7 @@ public class practice{
     }
     
     public static void main(String[] args){
-    int nums[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int nums[] = {-2, -3, -8, -7};
     System.out.println(subarrsum(nums));
     }
 }
