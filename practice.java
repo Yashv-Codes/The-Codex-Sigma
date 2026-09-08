bdeb169c5d23b7fe94b8c614258f5905e4191393
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 13:02:42 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class practice{
-    public static boolean freq
+    public static boolean freq(int nums[]){
+        
+    }
     
     
     public static void main(String[] args){
