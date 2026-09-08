137e1e6c38a68be16a8442854cb7686b45ad11a0
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 05:11:03 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,24 +1,8 @@
 import java.util.*;
 public class practice{
-    public static int maxSubarraysum(int nums[]){
-        int currsum = 0, maxSum = Integer.MIN_VALUE;
-        int prefix[] = new int[nums.length];
-        prefix[0] = nums[0];
-        for(int i=1; i<prefix.length; i++){
-            prefix[i] = prefix[i-1] + nums[i];
-        }
+    
 
-        for(int i=0; i<nums.length; i++){
-            for(int j=i; j<nums.length; j++){
-                currsum = i==0 ? prefix[j] : prefix[j] - prefix[i-1];
-                 if(currsum > maxSum)
-                    maxSum = currsum;
-
-                   
-            }
-           
-        }
-        return maxSum;
+        
         
         
     }
