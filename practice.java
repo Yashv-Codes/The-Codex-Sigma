eacf22dfec0f4fbd7a8d7045f0207ba381b122d5
Yashv-Codes <yashv5529@gmail.com>
2026-09-08 13:03:23 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class practice{
     public static boolean freq(int nums[]){
         for(int i=0; i<nums.length; i++){
-            
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[j] == nums[i])
+            }
         }
     }
     
