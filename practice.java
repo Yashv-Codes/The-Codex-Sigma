6bb3adc6e778d39b07891fbe8dc926c1a2d27b62
Yashv-Codes <yashv5529@gmail.com>
2026-09-10 07:32:58 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class practice {
     public static void bubble_sort(int nums[]){
         for(int i=0; i<nums.length-1; i++){
-            for(int j=0; )
+            for(int j=0; j<nums.length-1-i; j++)
         }
         
          
