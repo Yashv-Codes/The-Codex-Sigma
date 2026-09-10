7faf2322a89e9a4d27069f889d682df3223d3383
Yashv-Codes <yashv5529@gmail.com>
2026-09-10 07:29:46 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,9 +1,8 @@
 import java.util.Arrays;
 public class practice {
     public static boolean bubble_sort(int nums[]){
-        if(nums[] == Arrays.sort(nums))
-            return true;
-        return false;
+        
+         
         
     }
     
