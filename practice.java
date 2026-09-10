c1a288607c8b86c13c1291bc7641cc3078fd7348
Yashv-Codes <yashv5529@gmail.com>
2026-09-10 07:11:39 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,8 +5,8 @@ public class practice {
         for(int i=0; i<=n-2; i++){
             for(int j=0; j<=n-2-i; j++){
                 if(nums[j] > nums[j+1]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
                     nums[j] = temp;
                 }
             }
