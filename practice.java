e719cdf488a1a247f8111487da233992734f2c70
Yashv-Codes <yashv5529@gmail.com>
2026-09-10 07:36:56 +0000

    DSA

--- practice.java
+++ practice.java
@@ -10,7 +10,7 @@ public class practice {
                     nums[j+1] = temp;
                 }
                 swap++;
-                
+                if(swap <=0)
 
             }
         }
