3427352d0c3f037a68eb582c7b1c6327def1baea
Yashv-Codes <yashv5529@gmail.com>
2026-09-10 07:43:50 +0000

    DSA

--- practice.java
+++ practice.java
@@ -24,7 +24,7 @@ public class practice {
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5};
         bubble_sort(nums);
-        System.out.println(Arrays.toString(nums));
+        // System.out.println(Arrays.toString(nums));
         
     }
 }
