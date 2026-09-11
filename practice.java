9bb2aeee4e5b66e8d34e4c267d6455c84f061644
Yashv-Codes <yashv5529@gmail.com>
2026-09-11 12:40:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,20 +1,7 @@
 import java.util.Arrays;
 public class practice {
     public static void bubble_sort(int nums[]){
-        for(int i=0; i<nums.length-1; i++){
-            int swap = 0;
-            for(int j=0; j<nums.length-1-i; j++){
-                if(nums[j] > nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                    swap++;
-                }
-            }
-            if(swap == 0)
-                break;
-        }
-        System.out.println(Arrays.toString(nums));
+        
         
          
         
@@ -23,8 +10,7 @@ public class practice {
 
     public static void main(String[] args) {
         int nums[] = {1, 2, 3, 4, 5};
-        bubble_sort(nums);
-        // System.out.println(Arrays.toString(nums));
+        
         
     }
 }
