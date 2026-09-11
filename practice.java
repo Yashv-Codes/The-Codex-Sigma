4e44c87534456b8a7e7250b2cd53aba04364dbdd
Yashv-Codes <yashv5529@gmail.com>
2026-09-11 13:06:57 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,9 @@ public class practice {
     public static void selection_sort(int nums[]){
         for(int i=0; i<nums.length-1; i++){
             int minPos = i;
-            for(int j=i+1; )
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[minPos])
+            }
         }
         
         
