713c75d4b4cbc1ffbfe06e19d6d187ba191e35e1
Yashv-Codes <yashv5529@gmail.com>
2026-09-11 13:55:24 +0000

    DSA

--- practice.java
+++ practice.java
@@ -10,7 +10,7 @@ public class practice {
 
     public static void main(String[] args) {
         int nums[] = {5, 4, 1, 3, 2};
-        insertion_sort(nums);
+        // insertion_sort(nums);
         
         
         
