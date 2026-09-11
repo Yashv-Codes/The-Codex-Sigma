53da394bcb941e443ee528cd75e7e0200f804e34
Yashv-Codes <yashv5529@gmail.com>
2026-09-11 13:19:40 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,23 +1,14 @@
 import java.util.Arrays;
 public class practice {
     public static void selection_sort(int nums[]){
-        for(int i=0; i<nums.length-1; i++){
-            int minPos = i;
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[minPos] > nums[j])
-                    minPos = j;
-            }
-            int temp = nums[minPos];
-            nums[minPos] = nums[i];
-            nums[i] = temp;
-        }
+        
         System.out.println(Arrays.toString(nums));
     }
     
 
     public static void main(String[] args) {
         int nums[] = {5, 4, 1, 3, 2};
-        selection_sort(nums);
+        
         
         
         
