3e8241f2241b2760538cd84244f3eb182509fdce
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 06:10:04 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,11 @@ public class practice {
     public static void sort(int nums[]){
         for(int i=0; i<nums.length-1; i++){
             for(int j=i+1; j<nums.length; j++){
-                if(nums[i] > nums[j])
+                if(nums[i] > nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j]
+                }
             }
         }
     }
