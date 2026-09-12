b7cca9a2836a0a7090dd9d9d0d92769c6fe7be89
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 06:11:34 +0000

    DSA

--- practice.java
+++ practice.java
@@ -15,7 +15,7 @@ public class practice {
     
 
     public static void main(String[] args) {
-        int nums[] = {3,6,2,1,};
+        int nums[] = {3,6,2,1,8,7,4,5,3,1};
         sort(nums);
         
         
