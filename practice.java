0dd85974de1a84bae2882b98c38566eb389d5c98
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 06:09:24 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class practice {
     public static void sort(int nums[]){
-        for(int i=0; i<nums.length; i++){
+        for(int i=0; i<nums.length-1; i++){
             for(int j=i+1; j<nums.length; )
         }
     }
