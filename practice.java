1b90e65014d24cb7397b3ff1b8656626bb3a03ec
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 06:08:00 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,16 +1,10 @@
 import java.util.Arrays;
 public class practice {
-    public static void counting_sort(int nums[]){
-       
-    
-        
-        System.out.println(Arrays.toString(nums));
-    }
     
 
     public static void main(String[] args) {
         int nums[] = {5, 4, 1, 3, 2};
-        counting_sort(nums);
+        
         
         
         
