8722b193fe472fc038859e1369bb50d963e93f19
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 07:28:05 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,7 +4,9 @@ public class practice {
         for(int i=1; i<nums.length; i++){
             int curr = nums[i];
             int prev = i-1;
-            while(prev>=0 )
+            while(prev>=0 && nums[prev] > curr){
+                
+            }
         }
         System.out.println(Arrays.toString(nums));
     }
