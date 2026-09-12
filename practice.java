0a3002b31658752671b992bf1b73253a4e3a87c9
Yashv-Codes <yashv5529@gmail.com>
2026-09-12 06:55:55 +0000

    DSA

--- practice.java
+++ practice.java
@@ -11,7 +11,7 @@ public class practice {
             nums[minPos] = nums[i];
             nums[i] = temp;
         }
-        System.out.
+        System.out.println(Arrays.toString(nums));
     }
     
 
