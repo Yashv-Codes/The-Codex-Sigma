8cd4c16707c48af8f35f036b36afc1e93b42c777
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 05:24:55 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,7 +7,7 @@ public class practice {
             if(tar == nums[mid])
                 return mid;
             else if(tar < nums[mid])
-                
+                end = mid-1;
         }
         
         
