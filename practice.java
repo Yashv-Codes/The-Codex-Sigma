6d9c46b562756ec442e81150418f57d437e424ed
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 06:07:43 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
 public class practice {
     public static void largest(int nums[]){
+        int largest = nums[0];
         for(int i=0; i<nums.length; i++){
             if(nums[i] >= 0)
                 return;
         }
         for(int i=0; i<nums.length; i++){
-            
             if()
+            
+            
                 
             
     }
