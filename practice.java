897d13ab1f45c5bb4679810d5b6447d2ca22223e
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 06:05:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice {
     public static int largest(int nums[]){
         for(int i=0; i<nums.length; i++){
             if(nums[i] >= 0)
-                r
+                return
 
         }
     }
