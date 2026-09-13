45ea8b26e27240e8c1dab61ee6c0ce261a6db4bd
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 05:21:46 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 public class practice {
-    public static void sort(int nums[]){
+    public static void search(int nums[]){
         
         System.out.println(Arrays.toString(nums));
     }
@@ -8,7 +8,7 @@ public class practice {
 
     public static void main(String[] args) {
         int nums[] = {3,6,2,1,8,7,4,5,3,1};
-        sort(nums);
+        
         
         
         
