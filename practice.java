fe38ec0a1a450a33031ee046da0d51acc607e66e
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 05:26:02 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,17 +1,7 @@
 import java.util.Arrays;
 public class practice {
     public static int search(int nums[], int tar){
-        int si = 0, ei = nums.length-1;
-        while(si <= ei){
-            int mid = si+(ei-si)/2;
-            if(tar == nums[mid])
-                return mid;
-            else if(tar < nums[mid])
-                ei = mid-1;
-            else
-                si = mid+1;
-        }
-        return -1;
+        
         
         
     }
