ddab691ae80a969a77ae46994f355e004476e614
Yashv-Codes <yashv5529@gmail.com>
2026-09-13 06:14:28 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,24 +1,9 @@
 import java.util.Arrays;
 public class practice {
-    public static int largest(int nums[]){
-        int largest = nums[0];
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] >= 0)
-                return 0;
-            else if(nums[i] > largest)
-                largest = nums[i];
-        }
-        return largest;
-        
-        
-        
-        
-    }
     
 
     public static void main(String[] args) {
-        int nums[] = {-2,-5,-8,-3,-13};
-        System.out.println(largest(nums));
+        
         
         
         
