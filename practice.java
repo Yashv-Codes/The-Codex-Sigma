e22b8ab5e13bdd6e367f51d170ad0407dc9c0670
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 05:08:29 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class practice {
     public static boolean search(int matrix[][], int key){
+        int largest = Math.
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == key)
