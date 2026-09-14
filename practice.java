72cfbf10ca046cd95d2fcaab6b2ef250f77754f7
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 05:04:07 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class practice {
     public static boolean search(int matrix[][], int key){
         for(int i=0; i<matrix.length; i++){
-            
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j])
+            }
         }
     }
     public static void main(String[] args) {
