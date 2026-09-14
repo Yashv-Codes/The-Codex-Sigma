8fd1151559594af417ff34a099939f347c915509
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 04:30:40 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int matrix[][] = new int[3][3];
-        
+        for(int i=)
         
         
         
