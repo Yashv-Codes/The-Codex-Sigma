4160af9196523cd632cab5dc4bf323a679273161
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 04:31:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,9 +5,10 @@ public class practice {
         int matrix[][] = new int[3][3];
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                matrix[]
+                matrix[i][j] = sc.nextInt();
             }
         }
+        
         
         
         
