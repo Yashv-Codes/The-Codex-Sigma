fb24367e21545c938c50aee970fcd46cc8f13f1a
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 05:04:17 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,9 +3,11 @@ public class practice {
     public static boolean search(int matrix[][], int key){
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
-                if(matrix[i][j])
+                if(matrix[i][j] == key)
+                    return true;
             }
         }
+        return false;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
