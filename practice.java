01bfc978e1453ace7f1b1c9012224e334f23b5fd
Yashv-Codes <yashv5529@gmail.com>
2026-09-14 05:02:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class practice {
+    public static 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int matrix[][] = new int[3][3];
