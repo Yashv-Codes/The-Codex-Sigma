1553173bac22571e0d45d16fbe296b2af0b32143
Yashv-Codes <yashv5529@gmail.com>
2026-09-15 15:42:06 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice {
     
     public static void main(String[] args) {
-        int matrix[][] = {1,2,3,4}
+        int matrix[][] = {{1,2,3,4}
         
         
         
