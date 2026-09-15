15e96455f8b8f1eb6079a9082acd2f1726633c09
Yashv-Codes <yashv5529@gmail.com>
2026-09-15 15:50:35 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,7 +12,9 @@ public class practice {
             for(int j=ecol-1; j>=scol; j--){
                 System.out.print(matrix[erow][j]+" ");
             }
-            for(int i=erow-1; i>=srow+1; i--)
+            for(int i=erow-1; i>=srow+1; i--){
+                System.out.print(matrix[i][scol])
+            }
         }
 
     }
