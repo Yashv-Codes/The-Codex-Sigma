13e1d2ce1c967c04244a288307b57c8211b69e57
Yashv-Codes <yashv5529@gmail.com>
2026-09-15 15:45:32 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class practice {
     public static void spiral_matrix(int matrix[][]){
         int srow = 0, scol = 0, erow = matrix.length-1, ecol = matrix[0].length-1;
-        while(srow <= erow && scol <= ecol)
+        while(srow <= erow && scol <= ecol){
+            
+        }
 
     }
     
