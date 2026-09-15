80e87f208e2f43d6e8aa0dee8c1972c36f28bde9
Yashv-Codes <yashv5529@gmail.com>
2026-09-15 15:52:11 +0000

    DSA

--- practice.java
+++ practice.java
@@ -25,7 +25,7 @@ public class practice {
                           {5,6,7,8},
                           {9,10,11,12},
                           {13,14,15,16}};
-        
+        spiral_matrix(matrix);
         
         
         
