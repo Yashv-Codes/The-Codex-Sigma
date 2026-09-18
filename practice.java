690b9bf07ea6c8570405c89066ac9e4cc43322cd
Yashv-Codes <yashv5529@gmail.com>
2026-09-18 13:40:50 +0000

    DSA

--- practice.java
+++ practice.java
@@ -16,7 +16,7 @@ public class practice {
         int matrix[][] = {{1,2,3,4},
                           {5,6,7,8},
                           {9,10,11,12}};
-                          
+        System.out.println(diagonal_sum)
                           
         
         
