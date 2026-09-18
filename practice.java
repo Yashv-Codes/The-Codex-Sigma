59e710bec39a8ba25837b6fcc0375766aa56fd34
Yashv-Codes <yashv5529@gmail.com>
2026-09-18 13:42:27 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,15 +1,5 @@
 import java.util.*;
 public class practice {
-    public static int diagonal_sum(int matrix[][]){
-        int sum = 0;
-        for(int i=0; i<matrix.length; i++){
-            sum += matrix[i][i];
-            if(i != matrix.length-1-i)
-                sum += matrix[i][matrix.length-1-i];
-
-        }
-        return sum;
-    }
     
     
     public static void main(String[] args) {
