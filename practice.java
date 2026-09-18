d7caa72c90355345ec9ed1dc1bd6491def86b938
Yashv-Codes <yashv5529@gmail.com>
2026-09-18 13:40:35 +0000

    DSA

--- practice.java
+++ practice.java
@@ -9,8 +9,6 @@ public class practice {
 
         }
         return sum;
-        
-        
     }
     
     
@@ -18,6 +16,7 @@ public class practice {
         int matrix[][] = {{1,2,3,4},
                           {5,6,7,8},
                           {9,10,11,12}};
+                          
                           
         
         
