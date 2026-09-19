a4310fca4d305e7e88fb92d2a21f660649d994d4
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 14:28:44 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,6 +5,7 @@ public class practice {
    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.next();
         // String str = "yash";
         // String str1 = new String("verma");
         
