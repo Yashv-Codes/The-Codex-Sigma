7de5a1fc90c61d21f3c98ca49c10f432adffcbd3
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:14:58 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,10 +1,11 @@
 public class practice {
     public static int sum(int matrix[][]){
-        
+
     }
    
    
     public static void main(String[] args) {
+        int matrix[][] = {{}}
         
         
         
