aa5ade1c8025a861d45a8f09b7ec7387debba4b7
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:01:13 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
 public class practice {
    
     public static void main(String[] args) {
-        int matrix[][] = {4,7,8},
+        int matrix[][] = {4,7,8},{8,8,7}};
         
         
                           
