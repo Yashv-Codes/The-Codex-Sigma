dca269976230fbadd3ed438531dab87b9c8b9110
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 05:46:42 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
 public class practice {
     public static boolean search_key(int matrix[][], int key){
         int row = matrix.length-1, col = 0;
-        while()
+        while(row >= 0 && )
     }
     
     public static void main(String[] args) {
