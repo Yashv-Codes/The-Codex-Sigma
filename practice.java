0d0d0ac4eb2b9b885cf45496c03c93bbd238cb2d
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:16:32 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,9 @@
 public class practice {
     public static int sum(int matrix[][]){
         int sum = 0;
+        for(int i=1; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){}
+        }
 
     }
    
