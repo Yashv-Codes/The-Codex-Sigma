3d519be751926a358653c46607c865301f5b4729
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 05:49:57 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice {
         int row = matrix.length-1, col = 0;
         while(row >= 0 && col < matrix.length){
             if(matrix[row][col] == key){
-                System.out.print("key found at "(" + row "," + col ")"")
+                System.out.print("key found at "(" + row "," + col + ")");
 
             }
             else if(key < matrix[row][col])
