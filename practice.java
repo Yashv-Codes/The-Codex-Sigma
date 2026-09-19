62a537c9b7b7ac879aa03b5967642265c5fe0bec
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:17:07 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,12 +7,14 @@ public class practice {
             }
             return sum;
         }
+        
 
     }
    
    
     public static void main(String[] args) {
         int matrix[][] = {{1,4,9}, {11,4,3}, {2,2,3}};
+        System.out.println(sum)
         
         
         
