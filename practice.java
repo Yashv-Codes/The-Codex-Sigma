a3c06526e97af57e513620755e82d8370a4470b1
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:05:44 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,7 +12,7 @@ public class practice {
    
     public static void main(String[] args) {
         int matrix[][] = {{4,7,8},{8,8,7}};
-        System.out.println(count(matrix))
+        System.out.println(count(matrix));
         
         
                           
