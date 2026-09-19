83441a2ae4f3e441495ce1b10b1dbef3cffe76f8
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 05:53:58 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,27 +1,11 @@
 public class practice {
-    public static boolean search_key(int matrix[][], int key){
-        int row = matrix.length-1, col = 0;
-        while(row >= 0 && col < matrix.length){
-            if(matrix[row][col] == key){
-                System.out.print("key found at (" + row + "," + col + ")");
-                return true;
-            }
-            else if(key < matrix[row][col])
-                row--;
-            else{
-                col++;
-            }
-        }
-        return false;
-    }
-    
+   
     public static void main(String[] args) {
         int matrix[][] = {{10,20,30,40},
                           {15,25,35,45},
                           {27,29,37,48},
                           {32,33,39,50}};
-        int key = 30;
-        search_key(matrix,key);
+        
         
                           
         
