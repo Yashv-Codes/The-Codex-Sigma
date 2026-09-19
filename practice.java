cefa5ec67952701e5b039558ca081a9ea89ebc7e
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:05:08 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,11 @@
 public class practice {
     public static int count(int matrix[][]){
         int count = 0;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 7)
+            }
+        }
     }
    
     public static void main(String[] args) {
