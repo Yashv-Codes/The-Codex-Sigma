c985b6b243e1dc90b74258498ca4015b0c27d3e9
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 06:05:19 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,8 +4,10 @@ public class practice {
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 if(matrix[i][j] == 7)
+                    count++;
             }
         }
+        return count;
     }
    
     public static void main(String[] args) {
