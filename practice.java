f6e5d2430a668675e120cd8c0f0e025a11048673
Yashv-Codes <yashv5529@gmail.com>
2026-09-19 05:29:41 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,7 @@ public class practice {
     
     public static void main(String[] args) {
         int matrix[][] = {{10,20,30,40},
-                          {50,60,70,80},
+                          {15,25,35,45},
                           {9,10,11}};
         
                           
