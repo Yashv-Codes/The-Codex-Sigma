88050f073da293d08f90abf52d27354cec042fac
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:47:45 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,10 +1,7 @@
 import java.util.*;
 public class practice {
-    public static void letter(String str){
-        for(int i=0; i<str.length(); i++){
-            System.out.print(str.charAt(i)+" ");
-        }
-        System.out.println();
+    public static boolean letter(String str){
+        
     }
     
    
