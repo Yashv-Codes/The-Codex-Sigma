af83f9ec4147ceaaa4015f416ca10b4e6a24d7b2
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:18:09 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,9 +4,10 @@ public class practice {
    
    
     public static void main(String[] args) {
-    String str = "yash";
+    String str = "yash ";
     String str1 = "verma";
     String str2 = str + str1;
+    System.out.println(str2);
 
         
         
