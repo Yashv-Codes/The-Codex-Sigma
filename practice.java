f73fa7dedc2f8f9d03f3fc4e75c644e584fde1c7
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:48:20 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class practice {
     public static boolean isPalindrome(String str){
+        int n = 
         
     }
     
    
    
     public static void main(String[] args) {
-    String str = "Yash ";
+    String str = "madam";
     
 
         
