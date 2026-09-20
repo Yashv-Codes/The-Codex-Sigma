014c437324cb19412f2680d61671d919e74800d8
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 12:19:45 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,13 +3,14 @@ public class practice {
     public static String substr(String str, int left, int right){
         
 
+
     }
     
     
    
    
     public static void main(String[] args) {
-    String str = "yash";
+    String str = "yash verma";
     System.out.println(str.substring(0,3));
 
         
