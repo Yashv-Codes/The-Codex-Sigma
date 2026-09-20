50d5e69c8467e8082eb779cd6a875d3bc19aeb90
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 12:15:20 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice {
    
     public static void main(String[] args) {
     String str = "yash";
-    str.substring(0,4);
+    System.out.println(str.substring(0,4));
 
         
         
