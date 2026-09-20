7a587e4123121834405fcc81da7ca7774358211e
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 13:04:01 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,22 +1,14 @@
 import java.util.*;
 public class practice {
-    public static String largest(String str[]){
-        String largest = str[0];
-        for(int i=1; i<str.length; i++){
-            if(largest.compareTo(str[i]) < 0)
-                largest = str[i];
-        }
-        return largest;
-
-    }
+    
         
     
     
    
    
     public static void main(String[] args) {
-    String str[] = {"apple", "mango", "banana"};
-    System.out.println(largest(str));
+    
+    
 
         
         
