f55af369de1c9d846e6b32b71879ca4f172b8dd7
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:54:36 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,6 +5,7 @@ public class practice {
         int left = 0, right = str.length()-1;
         while(left < right){
             if(str.charAt(left) != str.charAt(right))
+                return false;
         }
         
     }
