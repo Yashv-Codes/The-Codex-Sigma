e2f223f13be04d3854393da792d5e698392c4853
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:51:04 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,10 @@ import java.util.*;
 public class practice {
     public static boolean isPalindrome(String str){
         int n = str.length();
-        int left = 0, right = str.length
+        int left = 0, right = str.length()-1;
+        while(left < right){
+            if(str.charAt(i) != str.charAt(n-1-i))
+        }
         
     }
     
