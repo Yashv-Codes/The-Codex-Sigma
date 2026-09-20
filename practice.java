699a53dbb56dc8209a2a8906ca63136d01097a2a
Yashv-Codes <yashv5529@gmail.com>
2026-09-20 05:49:09 +0000

    DSA

--- practice.java
+++ practice.java
@@ -3,7 +3,8 @@ public class practice {
     public static boolean isPalindrome(String str){
         int n = str.length();
         for(int i=0; i<str.length()/2; i++){
-            if(str.charAt)
+            if(str.charAt(i) != str.charAt(n-1-i))
+                return false;
         }
         
     }
