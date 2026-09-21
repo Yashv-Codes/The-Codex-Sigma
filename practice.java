9059b68af87cfa26d1cbb2fc13c4ac4978c662f4
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 07:15:26 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,20 +1,6 @@
 import java.util.*;
 public class practice {
-    public static String compression(String str){
-        StringBuilder sb = new StringBuilder("");
-        for(int i=0; i<str.length(); i++){
-            int count = 1;
-            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
-                count++; i++;
-            }
-            sb.append(str.charAt(i));
-            if(count > 1)
-                sb.append(count);
-        }
-        return sb.toString();
-
-        
-    }
+    
     
         
     
@@ -22,8 +8,7 @@ public class practice {
    
    
     public static void main(String[] args) {
-        String str = "aaabbcccdd";
-        System.out.println(compression(str));
+        
 
         
     
