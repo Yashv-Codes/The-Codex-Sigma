a69658d1a1fa4df777c8c770a29574a80e58abb4
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 07:03:51 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,7 +4,10 @@ public class practice {
         StringBuilder sb = new StringBuilder("");
         for(int i=0; i<str.length(); i++){
             int count = 1;
-            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1))
+            while(i < str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++; i++;
+            }
+            
         }
 
         
