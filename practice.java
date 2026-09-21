f1de32ba55fe6b4f5fb4c5d5ec8f3fff930b6826
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 05:10:31 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,11 +7,7 @@ public class practice {
    
    
     public static void main(String[] args) {
-    StringBuilder sb = new StringBuilder("");
-    for(int i=1; i<11; i++){
-        sb.append(i);
-    }
-    System.out.println(sb);
+    
     
     
 
