c8dc6e6346bf57962de7970e29d2ab8ff367e12f
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 06:34:54 +0000

    DSA

--- practice.java
+++ practice.java
@@ -5,11 +5,8 @@ public class practice {
         sb.append(Character.toUpperCase(str.charAt(0)));
         for(int i=1; i<str.length(); i++){
             if(str.charAt(i-1) == ' ' && str.charAt(i) != ' '){
-                sb.append(str.charAt(i));
-                i++;
-                sb.append(Character.toUpperCase(str.charAt(i)));
-
-            }
+                
+                sb.append(Character.toUpperCase(str.charAt(i)));}
             else
                 sb.append(str.charAt(i));
         }
