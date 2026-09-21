d17183aa035c514e84dbc8b2305a083af0ac0cec
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 06:36:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,15 +2,7 @@ import java.util.*;
 public class practice {
     public static String toUppercase(String str){
         StringBuilder sb = new StringBuilder("");
-        sb.append(Character.toUpperCase(str.charAt(0)));
-        for(int i=1; i<str.length(); i++){
-            if(str.charAt(i-1) == ' ' && str.charAt(i) != ' '){
-                sb.append(Character.toUpperCase(str.charAt(i)));
-            }
-            else
-                sb.append(str.charAt(i));
-        }
-        return sb.toString();
+        
     }
     
         
