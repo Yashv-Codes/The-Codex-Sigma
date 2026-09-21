94b9e16a856bd894cd47c4a45b9dce87a3e9a031
Yashv-Codes <yashv5529@gmail.com>
2026-09-21 07:02:23 +0000

    DSA

--- practice.java
+++ practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class practice {
     public static String compression(String str){
         StringBuilder sb = new StringBuilder("");
-        
+        for(int i=0; i<str.length(); i++)
 
         
     }
