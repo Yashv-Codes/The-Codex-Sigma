915c0b7a0fe68b8cc1c5c8f5d4bc78b97911a914
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 04:36:48 +0000

    DSA

--- practice.java
+++ practice.java
@@ -12,7 +12,7 @@ public class practice {
         int count = 0;
         for(int i=0; i<str.length(); i++){
             char ch = str.charAt(i);
-            if(ch == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u')
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || i == 'u')
                 count++;
         }
         System.out.println("total lowercase vowels  = "+count);
