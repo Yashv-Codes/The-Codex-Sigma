f5ab4c2b5276a2ca7541623de9b82ab03eeff97c
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 14:04:16 +0000

    DSA

--- practice.java
+++ practice.java
@@ -19,8 +19,9 @@ public class practice {
         }
         for(int i=0; i<26; i++){
             if(freq[i] != 0)
-                System.out.print("not anagram");
+                System.out.println("not anagram");
         }
+        System.out.println("Anagrams");
         
             
 
