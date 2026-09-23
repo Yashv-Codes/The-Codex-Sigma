2c507e6b1922f0f3b7350e842feecc996c534788
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 14:02:29 +0000

    DSA

--- practice.java
+++ practice.java
@@ -9,7 +9,10 @@ public class practice {
     public static void main(String[] args) {
         String s = "nap";
         String t = "pan";
-        
+        int freq[] = new int[26];
+        for(int i=0; i<26; i++){
+            freq[]
+        }
         
             
 
