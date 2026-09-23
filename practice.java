3a37f09317dcfcbc95a9099fe29af6401620b2c0
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 05:04:22 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 public class practice {
+    public static boolean anagrams(String str, String str1){
+
+    }
     
     
         
@@ -11,13 +14,7 @@ public class practice {
         
         String str = "race";
         String str1 = "care";
-        if(str.length() == str1.length()){
-            str.toLowerCase();
-            str1.toLowerCase();
-            char[] charArray = str.toCharArray();
-            char[] charArray1 = str.toCharArray();
-            Arrays.sort(charArray);
-            Arrays.sort(charArray1);
+        
             
 
         }
