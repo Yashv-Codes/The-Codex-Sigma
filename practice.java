a01a99f2961c73992503a09d1e57258ca06f9ad6
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 04:47:09 +0000

    DSA

--- practice.java
+++ practice.java
@@ -8,7 +8,7 @@ public class practice {
    
    
     public static void main(String[] args) {
-        String str = "ApnaCollege".replace("l", " ");
+        String str = "ApnaCollege".replace("l", "");
         System.out.println(str);
 
         
