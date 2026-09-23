47ab225f4db8410a3f85505235e48b7212343147
Yashv-Codes <yashv5529@gmail.com>
2026-09-23 04:45:59 +0000

    DSA

--- practice.java
+++ practice.java
@@ -9,9 +9,7 @@ public class practice {
    
     public static void main(String[] args) {
         String str = "ShradhaDidi";
-        String str1 = "ApnaCollege";
-        String str2 = "ShradhaDidi";
-        System.out.println(str.equals(str1)+" "+str.equals(str2));
+        
 
     }
 }
