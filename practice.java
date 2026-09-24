c424405b22af15be395dfd8ae769a1238cb55368
Yashv-Codes <yashv5529@gmail.com>
2026-09-24 05:03:31 +0000

    DSA

--- practice.java
+++ practice.java
@@ -7,23 +7,7 @@ public class practice {
 
     
     public static void main(String[] args) {
-        String s = "nap";
-        String t = "pan";
-        int freq[] = new int[26];
-        for(int i=0; i<s.length(); i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-
-            
-
-        }
-        for(int i=0; i<26; i++){
-            if(freq[i] != 0)
-                System.out.println("not anagram");
-        }
-        System.out.println("Anagrams");
         
-            
 
         }
         
