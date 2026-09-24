3acf24a9d25529fcc32a05b5c9c522adb31935b3
Yashv-Codes <yashv5529@gmail.com>
2026-09-24 15:26:23 +0000

    DSA

--- practice.java
+++ practice.java
@@ -8,8 +8,7 @@ public class practice {
 
     
     public static void main(String[] args) {
-        System.out.println(15 & (0<<3));
-        System.out.println(15 & (1<<2));
+        
         
 
         }
