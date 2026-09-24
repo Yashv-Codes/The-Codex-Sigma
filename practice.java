eda06997959f01e1cd08d8e52ee803b93820d3a8
Yashv-Codes <yashv5529@gmail.com>
2026-09-24 16:24:12 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,15 +1,9 @@
 import java.util.Arrays;
 public class practice {
-    public static int get_ithbit(int n, int i){
-        int bitmask = 1<<i;
-        if((n & bitmask) == 0)
-            return 0;
-        return 1;
-
-    }
+    
     
     public static void main(String[] args) {
-        System.out.println(get_ithbit(11,3));
+        
         
         
 
