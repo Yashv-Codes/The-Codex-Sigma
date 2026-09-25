7f6d6241f3a30928dd4e2b918ccc9dfe7e618d70
Yashv-Codes <yashv5529@gmail.com>
2026-09-25 06:09:53 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 public class practice {
-    public static clear_bit(int n)
+    public static clear_bit(int n){
+        int bitmask = ~(1<<i)
+    }
     
     
     public static void main(String[] args) {
