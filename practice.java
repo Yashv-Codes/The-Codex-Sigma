57ba1714bf2daf0a94b79ee078845afb881c87a4
Yashv-Codes <yashv5529@gmail.com>
2026-09-25 06:10:44 +0000

    DSA

--- practice.java
+++ practice.java
@@ -8,7 +8,7 @@ public class practice {
     
     
     public static void main(String[] args) {
-        System.out.println()
+        System.out.println(clear_bit(13,))
         
         
         
