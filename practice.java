9c8267101a45312ee9b055bf752406b8510aa8a4
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 06:20:12 +0000

    DSA

--- practice.java
+++ practice.java
@@ -13,7 +13,7 @@ public class practice {
     
     
     public static void main(String[] args) {
-        System.out.println(count_setbits(4));
+        System.out.println(count_setbits(15));
         
             
         
