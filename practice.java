992d9f85d9e9346d412cb4be6d268b0992ed8e0d
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 05:07:56 +0000

    DSA

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice {
     
     
     public static void main(String[] args) {
-        System.out.println()
+        System.out.println(odd_even(4));
             
         
         
