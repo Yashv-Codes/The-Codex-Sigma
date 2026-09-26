378a6de5234d69436f1cfeea3b58edca812b877b
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 05:05:59 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 public class practice {
-    public static voi
+    public static void odd_even(int n){
+        return n & 1 == 1;
+    }
     
     
     public static void main(String[] args) {
