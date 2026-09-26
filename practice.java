5fb85ef1c96ee30c5139d6030a304b3cb5ec2571
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 06:27:29 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,25 +1,11 @@
-import java.util.Arrays;
+
 public class practice {
-    public static int count_setbits(int n){
-        int count = 0;
-        while(n != 0){
-            if((n & 1) != 0)
-                count++;
-            n >>= 1;
-        }
-        return count;
-    }
+    
     
     
     
     public static void main(String[] args) {
-        int a = 1011101, count = 0;
-        String str = Integer.toString(a);
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) != 0)
-                count++;
-        }
-        System.out.println(count);
+        
         
             
         
