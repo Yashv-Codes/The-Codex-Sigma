8c726fa48371b94626510da9588eaf5645708a78
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 05:08:22 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 public class practice {
-    public static boolean odd_even(int n){
-        return (n & 1) == 1;
-    }
+    
     
     
     public static void main(String[] args) {
-        System.out.println(odd_even(5));
+        
             
         
         
