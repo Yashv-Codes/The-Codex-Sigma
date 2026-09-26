02d6b015004e465b1891dcc500c962301489d926
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 14:37:51 +0000

    DSA

--- practice.java
+++ practice.java
@@ -4,11 +4,7 @@ public class practice {
     
     
     public static void main(String[] args) {
-        int a = 5, b = 3;
-        a = a^b;
-        b = a^b;
-        a = a^b;
-        System.out.println(a + "," + b);
+        
         
         
             
