220658523c44d1fcb8ca20e4067c0647e5b98de0
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 06:24:26 +0000

    DSA

--- practice.java
+++ practice.java
@@ -13,8 +13,7 @@ public class practice {
     
     
     public static void main(String[] args) {
-        int arr[] = {1,2,3};
-        System.out.println(Arrays.toString());
+        
         
             
         
