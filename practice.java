1998dd0bac621dc47e7b1ace78acf19f308222dd
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 06:26:39 +0000

    DSA

--- practice.java
+++ practice.java
@@ -15,7 +15,7 @@ public class practice {
     public static void main(String[] args) {
         int a = 1011101, count = 0;
         String str = String.valueOf(a);
-        for(int i=0; i<String.length(); i++){
+        for(int i=0; i<str.length(); i++){
             if(str.charAt(i) != 0)
                 count++;
         }
