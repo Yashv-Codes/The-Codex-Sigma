431a18b5c74efa12a90a622721db23d8f13f6083
Yashv-Codes <yashv5529@gmail.com>
2026-09-26 05:32:49 +0000

    DSA

--- practice.java
+++ practice.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class practice {
-    public static int clear_bits(int n. int i){
-        return 
+    public static int clear_bits(int n, int i){
+        return n & (~0)<<i)
     }
     
     
